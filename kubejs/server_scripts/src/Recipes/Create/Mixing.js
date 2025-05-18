1280b40e7f73dc56a234b3f368dd847ebc607e61
hao888myt <1581375158@qq.com>
2025-05-18 21:36:12 +0800

    矿石饲料配方

--- kubejs/server_scripts/src/Recipes/Create/Mixing.js
+++ kubejs/server_scripts/src/Recipes/Create/Mixing.js
@@ -3,4 +3,7 @@ ServerEvents.recipes(event=>{
 
     //植本质
     create.mixing(Item.of('create:tree_fertilizer', 4), [Item.of('minecraft:bone_meal', 2), 'spectrum:vegetal'])
+
+    // 矿石饲料
+    create.mixing([Item.of('kubejs:ore_feed', 8), Item.of('kubejs:ore_feed', 2).withChance(0.5)], [Item.of('minecraft:coal', 2), Item.of('spelunkery:rough_cinnabar'), Item.of('create:crushed_raw_iron'), Item.of('spelunkery:rough_lazurite', 5), Item.of('create:crushed_raw_copper'), Item.of('minecraft:glow_berries', 8)])
 })
