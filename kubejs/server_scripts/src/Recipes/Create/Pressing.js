1e37ba8c4793c89e0418bc64f1bf2c9c503feac9
hao888myt <1581375158@qq.com>
2025-05-18 21:36:24 +0800

    树脂冲压机配方

--- kubejs/server_scripts/src/Recipes/Create/Pressing.js
+++ kubejs/server_scripts/src/Recipes/Create/Pressing.js
@@ -1,4 +1,7 @@
 ServerEvents.recipes(event => {
     const create = event.recipes.create
     create.pressing(Item.of('minecraft:bone_meal', 16), 'spectrum:vegetal')
+
+    // 去皮木头压成树脂
+    create.pressing([Item.of('anvilcraft:resin').withChance(0.5), Item.of('anvilcraft:wood_fiber')], "#forge:stripped_logs")
 })
