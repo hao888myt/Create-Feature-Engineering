764d37ca402c779a26736c6a364a8f98210f9ec4
hao888myt <1581375158@qq.com>
2025-05-18 21:38:20 +0800

    各种Tag

--- kubejs/server_scripts/src/Tags/BlockTags.js
+++ kubejs/server_scripts/src/Tags/BlockTags.js
@@ -1,11 +1,27 @@
 ServerEvents.tags("block", event => {
-    event.add('alexscaves:ferromagnetic_blocks', 'anvilcraft:hollow_magnet_block')
-    event.add('alexscaves:ferromagnetic_blocks', 'anvilcraft:magnet_block')
-    event.add('alexscaves:ferromagnetic_blocks', 'anvilcraft:ferrite_core_magnet_block')
-    event.add('alexscaves:ferromagnetic_blocks', 'quark:magnet')
+    let ferromagnetic_blocks = [
+        'anvilcraft:hollow_magnet_block',
+        'anvilcraft:magnet_block',
+        'anvilcraft:ferrite_core_magnet_block',
+        'quark:magnet',
+        'endergetic:boof_block'
+    ]
+    ferromagnetic_blocks.forEach(block => {
+        event.add('alexscaves:ferromagnetic_blocks', block)
+    })
 
+    // 苍绿催化体
     event.add('minecraft:needs_diamond_tool', 'kubejs:paltaeria_catalyst')
     event.add('minecraft:mineable/pickaxe', 'kubejs:paltaeria_catalyst')
+
+    // 乌龟化石
+    event.add('minecraft:needs_stone_tool', 'kubejs:toretoise_fossil')
+    event.add('minecraft:mineable/pickaxe', 'kubejs:toretoise_fossil')
+
+    // 各种深板岩矿
+    event.add("c:ores_in_ground/deepslate", "createnuclear:deepslate_lead_ore") // 原子核动的铅矿
+    event.add("c:ores_in_ground/deepslate", "createnuclear:deepslate_uranium_ore") // 原子核动的铀矿
+    event.add("c:ores_in_ground/deepslate", "create:deepslate_zinc_ore") // 机械动力的锌矿
 
     removeItemsFromTag(event, 'alexscaves:ferromagnetic_blocks', 'minecraft:anvil')
     removeItemsFromTag(event, 'alexscaves:ferromagnetic_blocks', 'minecraft:chipped_anvil')
