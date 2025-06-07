e6eb971ab5b95e11ea28ac980b33715468dae4fc
hao888myt <1581375158@qq.com>
2025-06-07 21:29:52 +0800

    水晶注液

--- kubejs/server_scripts/src/Recipes/Create/Filling.js
+++ kubejs/server_scripts/src/Recipes/Create/Filling.js
@@ -11,4 +11,18 @@ ServerEvents.recipes(event => {
     //以太石
     // event.remove('create_dd:filling/aethersite')
     // create.filling('create_dd:aethersite', [Fluid.of('create_dd:chromatic_waste', 50), '#forge:stone'])
+
+    // 水晶注液
+    FillingCrystal("citrine")
+    FillingCrystal("topaz")
+    function FillingCrystal(mateirial){
+      create.filling(Item.of(`spectrum:small_${mateirial}_bud`), [Item.of(`spectrum:${mateirial}_shard`), Fluid.of("minecraft:water", 250)])
+      create.filling(Item.of(`spectrum:medium_${mateirial}_bud`), [Item.of(`spectrum:small_${mateirial}_bud`), Fluid.of("minecraft:water", 250)])
+      create.filling(Item.of(`spectrum:large_${mateirial}_bud`), [Item.of(`spectrum:medium_${mateirial}_bud`), Fluid.of("minecraft:water", 250)])
+      create.filling(Item.of(`spectrum:${mateirial}_cluster`), [Item.of(`spectrum:large_${mateirial}_bud`), Fluid.of("minecraft:water", 250)])
+    }
+    create.filling(Item.of(`minecraft:small_amethyst_bud`), [Item.of(`minecraft:amethyst_shard`), Fluid.of("minecraft:water", 250)])
+    create.filling(Item.of(`minecraft:medium_amethyst_bud`), [Item.of(`minecraft:small_amethyst_bud`), Fluid.of("minecraft:water", 250)])
+    create.filling(Item.of(`minecraft:large_amethyst_bud`), [Item.of(`minecraft:medium_amethyst_bud`), Fluid.of("minecraft:water", 250)])
+    create.filling(Item.of(`minecraft:amethyst_cluster`), [Item.of(`minecraft:large_amethyst_bud`), Fluid.of("minecraft:water", 250)])
   })
