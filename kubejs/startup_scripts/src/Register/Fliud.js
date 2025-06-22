c0ee8f8ae9b2510048fb1beedb74be67b4defc99
hao888myt <1581375158@qq.com>
2025-06-22 12:37:02 +0800

    注册了灵质

--- kubejs/startup_scripts/src/Register/Fliud.js
+++ kubejs/startup_scripts/src/Register/Fliud.js
@@ -1,7 +1,5 @@
-// StartupEvents.registry('fluid', event => {
-//     // Basic "thick" (looks like lava) fluid with red tint
-//     event.create('thick_fluid')
-//       .thickTexture(0xFF0000)
-//       .bucketColor(0xFF0000)
-//       .displayName('Thick Fluid')
-// })
+StartupEvents.registry('fluid', event => {
+    event.create('ectoplasm')
+      .thickTexture(0x85e6ff)
+      .bucketColor(0x85e6ff)
+})
