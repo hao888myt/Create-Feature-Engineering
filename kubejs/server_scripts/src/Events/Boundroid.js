c56ba8b75dad51ce3ea5706d789a192ac9b4efd4
hao888myt <1581375158@qq.com>
2025-07-06 20:33:31 +0800

    弹跳机兵产磁铁（WIP）

---
+++ kubejs/server_scripts/src/Events/Boundroid.js
@@ -0,0 +1,6 @@
+// NativeEvents.onEvent("net.minecraftforge.event.entity.living.LivingFallEvent", event=>{
+//     const entity = event.getEntity()
+//     console.log(entity.getType())
+//     if (entity.getType() !== "alexscaves:boundroid") return
+//     console.log("Boundroid fall")
+// })
