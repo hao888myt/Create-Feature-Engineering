9b59fb0343cf924c8bc7ae0b21a1fc409b93cdd4
hao888myt <1581375158@qq.com>
2025-07-06 20:38:29 +0800

    注册了灵质、熔融钕合金，更改了灵质的材质

--- kubejs/startup_scripts/src/Register/Fliud.js
+++ kubejs/startup_scripts/src/Register/Fliud.js
@@ -1,5 +1,15 @@
 StartupEvents.registry('fluid', event => {
     event.create('ectoplasm')
-      .thickTexture(0x85e6ff)
+      .thinTexture(0x85e6ff)
       .bucketColor(0x85e6ff)
+
+    // 熔融青钕合金
+    event.create("molten_azure_neodymium")
+      .thickTexture(0x163cb8)
+      .bucketColor(0x163cb8)
+    
+    // 熔融赤钕合金
+    event.create("molten_scarlet_neodymium")
+      .thickTexture(0xd90f1f)
+      .bucketColor(0xd90f1f)
 })
