489838de8c1e22d7fffb3a4cd45209655b20b308
hao888myt <1581375158@qq.com>
2025-07-06 20:40:21 +0800

    添加了硬化树脂的配方

--- kubejs/server_scripts/src/Recipes/Create/Pressing.js
+++ kubejs/server_scripts/src/Recipes/Create/Pressing.js
@@ -4,4 +4,9 @@ ServerEvents.recipes(event => {
 
     // 去皮木头压成树脂
     create.pressing([Item.of('anvilcraft:resin').withChance(0.5), Item.of('anvilcraft:wood_fiber')], "#forge:stripped_logs")
+
+    // 硬化树脂
+    event.remove("anvilcraft:cook/hardend_resin")
+    create.pressing("anvilcraft:hardend_resin", "#kubejs:sap")
+
 })
