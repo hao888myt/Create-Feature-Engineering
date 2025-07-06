38fb1d4e8c6030f84780e357ad10f194d4315935
hao888myt <1581375158@qq.com>
2025-07-06 20:35:20 +0800

    Anvilcraft文件修改

--- kubejs/server_scripts/src/Recipes/AnvilCraft/AnvilCraft.js
+++ kubejs/server_scripts/src/Recipes/AnvilCraft/AnvilCraft.js
@@ -1,13 +1,7 @@
 ServerEvents.recipes(event =>{
 
     // SelectOneBuilder 样例用法，使用菌光体生成三种蛙明灯的其中一种
-    event.recipes.anvilcraft.anvil_processing('test3')
-        .hasItemIngredient('minecraft:shroomlight')
-        .selectOne(SelectOneBuilder.builder()
-            .spawnItem(0, 0, 0, 1.0, 'minecraft:ochre_froglight')
-            .spawnItem(0, 0, 0, 1.0, 'minecraft:verdant_froglight')
-            .spawnItem(0, 0, 0, 1.0, 'minecraft:pearlescent_froglight')
-        )
+    // event.recipes.anvilcraft.anvil_processing([], [])
 
     // event.recipes.anvilcraft.mob_transform('test2')
     //     .input('minecraft:sheep')
