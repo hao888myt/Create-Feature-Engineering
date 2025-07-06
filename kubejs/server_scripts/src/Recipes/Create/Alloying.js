a694ad10a8b39ca9a4bb515bb6fcafa3bc123ad0
hao888myt <1581375158@qq.com>
2025-07-06 20:36:23 +0800

    添加了钕合金的熔铸配方

---
+++ kubejs/server_scripts/src/Recipes/Create/Alloying.js
@@ -0,0 +1,17 @@
+ServerEvents.recipes(event => {
+    const createmetallurgy = event.recipes.createmetallurgy
+
+    // 熔融青钕合金
+    createmetallurgy.alloying(Fluid.of("kubejs:molten_azure_neodymium", 90), 
+        [
+            Item.of("alexscaves:raw_azure_neodymium", 1),
+            Fluid.of("createmetallurgy:molten_iron", 135)
+    ])
+
+    // 熔融赤钕合金
+    createmetallurgy.alloying(Fluid.of("kubejs:molten_scarlet_neodymium", 90), 
+        [
+            Item.of("alexscaves:raw_scarlet_neodymium", 1),
+            Fluid.of("createmetallurgy:molten_iron", 135)
+    ])
+})
