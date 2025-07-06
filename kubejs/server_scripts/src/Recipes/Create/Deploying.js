52fb78185c3902de15eb195a726cdb9e492ef767
hao888myt <1581375158@qq.com>
2025-07-06 20:37:14 +0800

    添加了磁流核做电子管的配方

--- kubejs/server_scripts/src/Recipes/Create/Deploying.js
+++ kubejs/server_scripts/src/Recipes/Create/Deploying.js
@@ -1,6 +1,6 @@
 ServerEvents.recipes(event => {
     const create = event.recipes.create
 
-    //未完成的异彩化合物
-    // create.deploying('kubejs:incomplete_chromatic_compound', ['create_dd:compound_base', '#create_dd:stone_types/aethersite'])
+    // 磁流核电子管
+    create.deploying(Item.of("create:electron_tube", 8), ["alexscaves:telecore", "create:polished_rose_quartz"])
 })
