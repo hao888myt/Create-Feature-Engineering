22277a81c483696316fd7db9c37dd68ce5a9c2b2
hao888myt <1581375158@qq.com>
2025-08-06 11:45:43 +0800

    粉碎轮配方

--- kubejs/server_scripts/src/Recipes/Create/MechanicalCrafting.js
+++ kubejs/server_scripts/src/Recipes/Create/MechanicalCrafting.js
@@ -13,81 +13,105 @@ ServerEvents.recipes(event => {
       C: 'immersive_aircraft:engine',
     })
 
+    // 粉碎轮
+    event.remove("create:mechanical_crafting/crushing_wheel")
+    create.mechanical_crafting(Item.of('create:crushing_wheel', 2), [
+      " SSS ",
+      "SSPSS",
+      "SPAPS",
+      "SSPSS",
+      " SSS "
+    ], {
+      A: 'create:andesite_alloy',
+      S: 'minecraft:smooth_stone',
+      P: '#minecraft:planks'
+    })
+
+    // 发信线圈
+    event.remove("create:crafting/materials/transmitter")
+    create.mechanical_crafting(Item.of('create:transmitter', 2), [
+      'E',
+      'C'
+    ], {
+      C: 'create_optical:copper_coil',
+      E: 'create:electron_tube'
+    })
+
     //磁电核心
-    event.remove('anvilcraft:magnetoelectric_core_forge')
-    event.remove('anvilcraft:magnetoelectric_core')
-    create.mechanical_crafting('anvilcraft:magnetoelectric_core', [
-      'ABA',
-      'CDC',
-      'ABA'
-    ], {
-      A: '#forge:ingots/copper',
-      B: '#c:glass_blocks',
-      C: 'kubejs:magnet_mechanism',
-      D: 'anvilcraft:hollow_magnet_block'
-    })
+    // event.remove('anvilcraft:magnetoelectric_core_forge')
+    // event.remove('anvilcraft:magnetoelectric_core')
+    // create.mechanical_crafting('anvilcraft:magnetoelectric_core', [
+    //   'ABA',
+    //   'CDC',
+    //   'ABA'
+    // ], {
+    //   A: '#forge:ingots/copper',
+    //   B: '#c:glass_blocks',
+    //   C: 'kubejs:magnet_mechanism',
+    //   D: 'anvilcraft:hollow_magnet_block'
+    // })
 
-    //集电器
-    event.remove('anvilcraft:charge_collector')
-    create.mechanical_crafting('anvilcraft:charge_collector', [
-      ' A ',
-      'B B',
-      'CCC'
-    ], {
-      A: 'anvilcraft:magnetoelectric_core',
-      B: '#forge:ingots/copper',
-      C: '#forge:ingots/steel'
-    })
+    // //集电器
+    // event.remove('anvilcraft:charge_collector')
+    // create.mechanical_crafting('anvilcraft:charge_collector', [
+    //   ' A ',
+    //   'B B',
+    //   'CCC'
+    // ], {
+    //   A: 'anvilcraft:magnetoelectric_core',
+    //   B: '#forge:ingots/copper',
+    //   C: '#forge:ingots/steel'
+    // })
 
-    //加热器
-    // event.remove('anvilcraft:heater')
+    // //加热器
+    // // event.remove('anvilcraft:heater')
+    // // create.mechanical_crafting(
+    // //   Item.of('anvilcraft:heater'),
+    // //   [
+    // //     'CCC',
+    // //     'BAB',
+    // //     'DDD'
+    // //   ],
+    // //   {
+    // //     A: 'anvilcraft:magnetoelectric_core',
+    // //     B: 'create_dd:infernal_mechanism',
+    // //     C: '#forge:wires/iron',
+    // //     D: '#forge:ingots/steel'
+    // //   }
+    // // )
+
+    // //压电晶体
+    // event.remove({ output: 'anvilcraft:piezoelectric_crystal' })
     // create.mechanical_crafting(
-    //   Item.of('anvilcraft:heater'),
+    //   Item.of('anvilcraft:piezoelectric_crystal', 3),
     //   [
-    //     'CCC',
-    //     'BAB',
-    //     'DDD'
+    //     'ABA',
+    //     'CDC',
+    //     'ABA'
     //   ],
     //   {
-    //     A: 'anvilcraft:magnetoelectric_core',
-    //     B: 'create_dd:infernal_mechanism',
-    //     C: '#forge:wires/iron',
-    //     D: '#forge:ingots/steel'
+    //     A: '#forge:ingots/copper',
+    //     B: '#forge:storage_blocks/amethyst',
+    //     C: '#vintageimprovements:springs/steel',
+    //     D: 'anvilcraft:magnetoelectric_core'
     //   }
     // )
 
-    //压电晶体
-    event.remove({ output: 'anvilcraft:piezoelectric_crystal' })
-    create.mechanical_crafting(
-      Item.of('anvilcraft:piezoelectric_crystal', 3),
-      [
-        'ABA',
-        'CDC',
-        'ABA'
-      ],
-      {
-        A: '#forge:ingots/copper',
-        B: '#forge:storage_blocks/amethyst',
-        C: '#vintageimprovements:springs/steel',
-        D: 'anvilcraft:magnetoelectric_core'
-      }
-    )
-
-    //输电杆
-    event.remove({ output: 'anvilcraft:transmission_pole' })
-    create.mechanical_crafting(
-      Item.of('anvilcraft:transmission_pole', 2),
-      [
-        'A',
-        'B',
-        'C'
-      ],
-      {
-        A: 'anvilcraft:magnetoelectric_core',
-        B: 'minecraft:lightning_rod',
-        C: '#forge:storage_blocks/iron'
-      }
-    )
+    // //输电杆
+    // event.remove({ output: 'anvilcraft:transmission_pole' })
+    // create.mechanical_crafting(
+    //   Item.of('anvilcraft:transmission_pole', 2),
+    //   [
+    //     'A',
+    //     'B',
+    //     'C'
+    //   ],
+    //   {
+    //     A: 'anvilcraft:magnetoelectric_core',
+    //     B: 'minecraft:lightning_rod',
+    //     C: '#forge:storage_blocks/iron'
+    //   }
+    // )
 
     //离心机
     event.remove('vintageimprovements:craft/centrifuge')
