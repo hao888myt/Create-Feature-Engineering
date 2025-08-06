470209c183df50c1a258a6e163045012857a4a55
hao888myt <1581375158@qq.com>
2025-08-06 11:42:56 +0800

    诅咒金打雷

---
+++ kubejs/server_scripts/src/Events/Lightenning.js
@@ -0,0 +1,51 @@
+ServerEvents.recipes(event => {
+    event.custom({
+        "type": "lychee:block_crushing",
+        "falling_block": "anvilcraft:royal_anvil",
+        "item_in": [
+            {
+                "item": 'anvilcraft:cursed_gold_ingot'
+            }
+        ],
+        "post": [
+            {
+                "type": "execute",
+                "command": 'summon minecraft:lightning_bolt'
+            }
+        ]
+    })
+
+    event.custom({
+        "type": "lychee:block_crushing",
+        "contextual": {
+        "type": "location",
+        "offsetY": -1,
+        "predicate": {
+            "block": {
+                    "blocks": [
+                        'anvilcraft:royal_anvil', 
+                        'anvilcraft:royal_smithing_table', 
+                        'anvilcraft:royal_steel_block', 
+                        'anvilcraft:smooth_royal_steel_block', 
+                        'anvilcraft:cut_royal_steel_block', 
+                        'anvilcraft:cut_royal_steel_pillar', 
+                        'anvilcraft:cut_royal_steel_slab', 
+                        'anvilcraft:cut_royal_steel_stairs',
+                        'anvilcraft:royal_grindstone'
+                    ]
+                }
+            }   
+        },
+        "item_in": [
+            {
+                "item": 'anvilcraft:cursed_gold_ingot'
+            }
+        ],
+        "post": [
+            {
+                "type": "execute",
+                "command": 'summon minecraft:lightning_bolt'
+            }
+        ]
+    })
+})
