ccf490c636e37d2227c33cad60e4345fe26947b0
hao888myt <1581375158@qq.com>
2025-08-06 11:49:24 +0800

    全局randomNum函数

--- kubejs/server_scripts/src/Events/Global.js
+++ kubejs/server_scripts/src/Events/Global.js
@@ -11,3 +11,14 @@ global.EnchantableMachines = [
     'create:mechanical_plough',
     'create:mechanical_drill'
 ]
+
+global.randomNum = (minNum, maxNum) => { 
+    switch(arguments.length){ 
+        case 1: 
+            return parseInt(Math.random() * minNum + 1, 10)
+        case 2: 
+            return parseInt(Math.random() * (maxNum - minNum + 1) + minNum, 10)
+        default: 
+            return 0;
+    } 
+} 
