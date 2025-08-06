de28539d8e55dcbb7d7792a729088679396c25d9
hao888myt <1581375158@qq.com>
2025-08-06 11:44:32 +0800

    注册了钢机壳

--- kubejs/startup_scripts/src/Register/Block.js
+++ kubejs/startup_scripts/src/Register/Block.js
@@ -1,3 +1,6 @@
+const $CasingBlock = Java.loadClass("com.simibubi.create.content.decoration.encasing.CasingBlock")
+const $EncasedCTBehaviour = Java.loadClass("com.simibubi.create.content.decoration.encasing.EncasedCTBehaviour")
+const $EncasingRegistry = Java.loadClass("com.simibubi.create.content.decoration.encasing.EncasingRegistry")
 StartupEvents.registry("block", event => {
 
     // 遍历设备名称并注册
@@ -9,7 +12,6 @@ StartupEvents.registry("block", event => {
             .tagBlock("create:wrench_pickup")
             .tagBlock("minecraft:mineable/pickaxe")
     })
-
     event.create(`brass_machine`)
             .model(`kubejs:block/brass_machine`)
             .hardness(3.0)
@@ -25,4 +27,16 @@ StartupEvents.registry("block", event => {
     event.create("toretoise_fossil").hardness(3.0).stoneSoundType() // 乌龟化石
 
     event.create("stradpole_skeleton").hardness(3.0).stoneSoundType() // 跨座蝌蚪遗骸
+
+    event.create("steel_casing") // 钢机壳
+
+    // event.createCustom("steel_casing", () => 
+    //     new $CasingBlock())
+
+    // event.createCustom('an_item:ranshao_qiti_gas', () =>
+    // new $Gas($GasBuilder.builder()
+    //   .tint(Color.of('#787878ff').getRgbJS())
+    //   .with(new $Fuel(() => 1, () => //1表示燃烧时间1t
+    //     $FloatingLong.create(200)) //200代表能量密度80FE/mB
+    //   )))
 })
