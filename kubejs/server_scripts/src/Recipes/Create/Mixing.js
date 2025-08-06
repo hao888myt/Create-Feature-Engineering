21f4e6a34dee468c32c3f5ffb5f5debb2bc465f0
hao888myt <1581375158@qq.com>
2025-08-06 11:46:56 +0800

    纸浆配方

--- kubejs/server_scripts/src/Recipes/Create/Mixing.js
+++ kubejs/server_scripts/src/Recipes/Create/Mixing.js
@@ -1,6 +1,9 @@
 ServerEvents.recipes(event=>{
     const create = event.recipes.create
 
+    // 纸浆
+    create.mixing(Item.of('create:pulp', 4), [Item.of('anvilcraft:wood_fiber', 2), Fluid.of('minecraft:water', 250)])
+
     //植本质
     create.mixing(Item.of('create:tree_fertilizer', 4), [Item.of('minecraft:bone_meal', 2), 'spectrum:vegetal'])
 
