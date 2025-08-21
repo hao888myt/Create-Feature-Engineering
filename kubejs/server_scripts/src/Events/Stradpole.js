0b12b94eb7ab3522974c2c1c8e3e060271be156b
hao888myt <1581375158@qq.com>
2025-08-22 00:28:48 +0800

    删除测试代码

--- kubejs/server_scripts/src/Events/Stradpole.js
+++ kubejs/server_scripts/src/Events/Stradpole.js
@@ -3,7 +3,6 @@ EntityEvents.death("alexsmobs:stradpole", event =>{
     const stradpole = event.entity
     const level = stradpole.level
     const pos = stradpole.getOnPos()
-    console.log(`Stradpole at ${pos} was killed by an anvil!`)
     var block_below = level.getBlock(pos.below(0))
     if (block_below.id != "minecraft:netherrack") return
     level.setBlockAndUpdate(pos.below(0), Block.getBlock("kubejs:stradpole_skeleton").defaultBlockState())
