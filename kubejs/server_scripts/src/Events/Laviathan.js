37ea04888e7a4f154b4a9088c924ba81a28a8855
hao888myt <1581375158@qq.com>
2025-08-22 00:25:40 +0800

    熔座砸黑曜石

---
+++ kubejs/server_scripts/src/Events/Laviathan.js
@@ -0,0 +1,33 @@
+EntityEvents.hurt("alexsmobs:laviathan", event => {
+    const source = event.getSource()
+    if (source == null) return
+    const actual = source.getActual()
+    if (actual == null) return
+
+    const laviathan = event.getEntity()
+
+    if (laviathan.nbt.getByte("Obsidian").toString() == "0") return
+    
+    else if (source.getType() == "anvil")
+    {
+        if (!laviathan.inWaterOrBubble)
+        {
+            const pos = laviathan.getOnPos()
+            const block = event.getLevel().getBlock(pos.above(3))
+            block.popItem(Item.of("obsidian", randomNum(8, 12)))
+            laviathan.mergeNbt("{Obsidian:0b}")
+        }
+        event.cancel()
+    }
+})
+
+function randomNum(minNum, maxNum){ 
+    switch(arguments.length){ 
+        case 1: 
+            return parseInt(Math.random() * minNum + 1, 10)
+        case 2:    
+            return parseInt(Math.random() * (maxNum - minNum + 1) + minNum, 10)
+        default: 
+            return 0;
+    } 
+}  
