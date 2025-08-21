8255ba16d3851c0246e25c29927de5ef4b844ea7
hao888myt <1581375158@qq.com>
2025-08-22 00:27:27 +0800

    诅咒金打雷自动化

--- kubejs/server_scripts/src/Events/Lightenning.js
+++ kubejs/server_scripts/src/Events/Lightenning.js
@@ -4,13 +4,13 @@ ServerEvents.recipes(event => {
         "falling_block": "anvilcraft:royal_anvil",
         "item_in": [
             {
-                "item": 'anvilcraft:cursed_gold_ingot'
+                "item": "anvilcraft:cursed_gold_ingot"
             }
         ],
         "post": [
             {
                 "type": "execute",
-                "command": 'summon minecraft:lightning_bolt'
+                "command": "summon minecraft:lightning_bolt"
             }
         ]
     })
@@ -23,28 +23,28 @@ ServerEvents.recipes(event => {
         "predicate": {
             "block": {
                     "blocks": [
-                        'anvilcraft:royal_anvil', 
-                        'anvilcraft:royal_smithing_table', 
-                        'anvilcraft:royal_steel_block', 
-                        'anvilcraft:smooth_royal_steel_block', 
-                        'anvilcraft:cut_royal_steel_block', 
-                        'anvilcraft:cut_royal_steel_pillar', 
-                        'anvilcraft:cut_royal_steel_slab', 
-                        'anvilcraft:cut_royal_steel_stairs',
-                        'anvilcraft:royal_grindstone'
+                        "anvilcraft:royal_anvil", 
+                        "anvilcraft:royal_smithing_table", 
+                        "anvilcraft:royal_steel_block", 
+                        "anvilcraft:smooth_royal_steel_block", 
+                        "anvilcraft:cut_royal_steel_block", 
+                        "anvilcraft:cut_royal_steel_pillar", 
+                        "anvilcraft:cut_royal_steel_slab", 
+                        "anvilcraft:cut_royal_steel_stairs",
+                        "anvilcraft:royal_grindstone"
                     ]
                 }
             }   
         },
         "item_in": [
             {
-                "item": 'anvilcraft:cursed_gold_ingot'
+                "item": "anvilcraft:cursed_gold_ingot"
             }
         ],
         "post": [
             {
                 "type": "execute",
-                "command": 'summon minecraft:lightning_bolt'
+                "command": "summon minecraft:lightning_bolt"
             }
         ]
     })
