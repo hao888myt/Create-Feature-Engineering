3e7324f9fe6f0945f4035898f7db320c6c702ade
hao888myt <1581375158@qq.com>
2025-09-20 22:28:58 +0800

    粉烧锭

--- kubejs/server_scripts/src/Recipes/Minecraft/Smelting.js
+++ kubejs/server_scripts/src/Recipes/Minecraft/Smelting.js
@@ -9,4 +9,15 @@ ServerEvents.recipes(event =>{
     event.remove("spelunkery:iron_from_magnetite_blasting")
     event.smelting("anvilcraft:magnet_ingot", "spelunkery:raw_magnetite").xp(0.35)
     event.blasting("anvilcraft:magnet_ingot", "spelunkery:raw_magnetite").xp(0.35)
+
+    let dust_to_ingot = [
+        ["#forge:dusts/iron", "minecraft:iron_ingot"],
+        ["#forge:dusts/gold", "minecraft:gold_ingot"],
+        ["#forge:dusts/copper", "minecraft:copper_ingot"],
+        ["#forge:dusts/zinc", "create:zinc_ingot"],
+        ["#forge:dusts/tungsten", "anvilcraft:tungsten_ingot"],
+    ]
+    dust_to_ingot.forEach(pair => {
+        event.smelting(pair[1], pair[0]).xp(0.7)
+    })
 })
