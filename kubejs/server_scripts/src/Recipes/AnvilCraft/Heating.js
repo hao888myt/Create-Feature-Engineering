9df49149497f144629e173694808a112f580ce6b
hao888myt <1581375158@qq.com>
2025-09-20 22:29:26 +0800

    粉双倍量产

--- kubejs/server_scripts/src/Recipes/AnvilCraft/Heating.js
+++ kubejs/server_scripts/src/Recipes/AnvilCraft/Heating.js
@@ -144,4 +144,82 @@ ServerEvents.recipes(event =>{
           }
         ]
       })
+    
+
+    let dust_to_ingot = [
+        ["forge:dusts/iron", "minecraft:iron_ingot"],
+        ["forge:dusts/gold", "minecraft:gold_ingot"],
+        ["forge:dusts/copper", "minecraft:copper_ingot"],
+        ["forge:dusts/zinc", "create:zinc_ingot"],
+        ["forge:dusts/tungsten", "anvilcraft:tungsten_ingot"],
+    ]
+    dust_to_ingot.forEach(pair => {
+        event.custom({
+            "type": "anvilcraft:anvil_processing",
+            "anvil_recipe_type": "super_heating",
+            "icon": {
+              "item": pair[1]
+            },
+            "outcomes": [
+              {
+                "type": "spawn_item",
+                "chance": 1.0,
+                "offset": [
+                  0.0,
+                  -1.0,
+                  0.0
+                ],
+                "result": {
+                  "count": 2,
+                  "item": pair[1]
+                }
+              }
+            ],
+            "predicates": [
+              {
+                "type": "has_block",
+                "match_block": {
+                  "blocks": [
+                    "anvilcraft:heater"
+                  ],
+                  "state": {
+                    "overload": "false"
+                  }
+                },
+                "offset": [
+                  0.0,
+                  -2.0,
+                  0.0
+                ]
+              },
+              {
+                "type": "has_block",
+                "match_block": {
+                  "blocks": [
+                    "minecraft:cauldron"
+                  ]
+                },
+                "offset": [
+                  0.0,
+                  -1.0,
+                  0.0
+                ]
+              },
+              {
+                "type": "has_item_ingredient",
+                "match_item": {
+                  "count": {
+                    "min": 1
+                  },
+                  "tag": pair[0]
+                },
+                "offset": [
+                  0.0,
+                  -1.0,
+                  0.0
+                ]
+              }
+            ]
+          })
+    })
 })
