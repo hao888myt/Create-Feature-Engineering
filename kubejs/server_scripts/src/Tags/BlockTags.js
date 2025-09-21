bff02669789bdacb47c8340351f4dee81797ee0f
hao888myt <1581375158@qq.com>
2025-09-21 22:44:47 +0800

    抽屉的tag

--- kubejs/server_scripts/src/Tags/BlockTags.js
+++ kubejs/server_scripts/src/Tags/BlockTags.js
@@ -29,6 +29,57 @@ ServerEvents.tags("block", event => {
         event.add("alexscaves:ferromagnetic_blocks", block)
     })
 
+    let drawers = [
+        "functionalstorage:oak_1",
+        "functionalstorage:spruce_1",
+        "functionalstorage:birch_1",
+        "functionalstorage:jungle_1",
+        "functionalstorage:acacia_1",
+        "functionalstorage:dark_oak_1",
+        "functionalstorage:crimson_1",
+        "functionalstorage:warped_1",
+        "functionalstorage:mangrove_1",
+        "functionalstorage:cherry_1",
+        "functionalstorage:framed_1",
+        "functionalstorage:fluid_1",
+
+        "functionalstorage:oak_2",
+        "functionalstorage:spruce_2",
+        "functionalstorage:birch_2",
+        "functionalstorage:jungle_2",
+        "functionalstorage:acacia_2",
+        "functionalstorage:dark_oak_2",
+        "functionalstorage:crimson_2",
+        "functionalstorage:warped_2",
+        "functionalstorage:mangrove_2",
+        "functionalstorage:cherry_2",
+        "functionalstorage:framed_2",
+        "functionalstorage:fluid_2",
+
+        "functionalstorage:oak_4",
+        "functionalstorage:spruce_4",
+        "functionalstorage:birch_4",
+        "functionalstorage:jungle_4",
+        "functionalstorage:acacia_4",
+        "functionalstorage:dark_oak_4",
+        "functionalstorage:crimson_4",
+        "functionalstorage:warped_4",
+        "functionalstorage:mangrove_4",
+        "functionalstorage:cherry_4",
+        "functionalstorage:framed_4",
+        "functionalstorage:fluid_4",
+
+        "functionalstorage:compacting_drawer",
+        "functionalstorage:compacting_framed_drawer",
+        "functionalstorage:simple_compacting_drawer",
+        "functionalstorage:framed_simple_compacting_drawer",
+        "functionalstorage:armory_cabinet",
+        "functionalstorage:ender_drawer",
+    ]
+    drawers.forEach(drawer => {
+        event.add("kubejs:drawer", drawer)
+    })
+
     // 苍绿催化体
     event.add("minecraft:needs_diamond_tool", "kubejs:paltaeria_catalyst")
     event.add("minecraft:mineable/pickaxe", "kubejs:paltaeria_catalyst")
