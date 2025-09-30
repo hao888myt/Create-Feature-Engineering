a4d24cf3e8bac49c08efb222a06feb913831f0fd
hao888myt <1581375158@qq.com>
2025-09-30 21:55:13 +0800

    精简相关代码，并调高了彩色树叶出货率

--- kubejs/server_scripts/src/Recipes/AnvilCraft/BlockSmash.js
+++ kubejs/server_scripts/src/Recipes/AnvilCraft/BlockSmash.js
@@ -34,7 +34,7 @@ ServerEvents.recipes(event => {
                   0.0
                 ],
                 "result": {
-                  "block": 'minecraft:air'
+                  "block": "minecraft:air"
                 }
               }
             ],
@@ -98,29 +98,15 @@ ServerEvents.recipes(event => {
     }
 
     //光谱世界树叶处理
-    var slaping_chance = 0.15
-    smashLeaves('spectrum:white_leaves', 'spectrum:white_sapling', slaping_chance)
-    smashLeaves('spectrum:orange_leaves', 'spectrum:orange_sapling', slaping_chance)
-    smashLeaves('spectrum:magenta_leaves', 'spectrum:magenta_sapling', slaping_chance)
-    smashLeaves('spectrum:light_blue_leaves', 'spectrum:light_blue_sapling', slaping_chance)
-    smashLeaves('spectrum:yellow_leaves', 'spectrum:yellow_sapling', slaping_chance)
-    smashLeaves('spectrum:lime_leaves', 'spectrum:lime_sapling', slaping_chance)
-    smashLeaves('spectrum:pink_leaves', 'spectrum:pink_sapling', slaping_chance)
-    smashLeaves('spectrum:gray_leaves', 'spectrum:gray_sapling', slaping_chance)
-    smashLeaves('spectrum:light_gray_leaves', 'spectrum:light_gray_sapling', slaping_chance)
-    smashLeaves('spectrum:cyan_leaves', 'spectrum:cyan_sapling', slaping_chance)
-    smashLeaves('spectrum:purple_leaves', 'spectrum:purple_sapling', slaping_chance)
-    smashLeaves('spectrum:blue_leaves', 'spectrum:blue_sapling', slaping_chance)
-    smashLeaves('spectrum:brown_leaves', 'spectrum:brown_sapling', slaping_chance)
-    smashLeaves('spectrum:green_leaves', 'spectrum:green_sapling', slaping_chance)
-    smashLeaves('spectrum:red_leaves', 'spectrum:red_sapling', slaping_chance)
-    smashLeaves('spectrum:black_leaves', 'spectrum:black_sapling', slaping_chance)
+    COLORS.forEach((color) => {
+      smashLeaves(`spectrum:${color}_leaves`, `spectrum:${color}_sapling`, 0.2)
+    })
 
     event.custom({
         "type": "anvilcraft:anvil_processing",
         "anvil_recipe_type": "block_smash",
         "icon": {
-          "item": 'minecraft:bedrock'
+          "item": "minecraft:bedrock"
         },
         "outcomes": [
           {
@@ -132,7 +118,7 @@ ServerEvents.recipes(event => {
               0.0
             ],
             "result": {
-              "block": 'minecraft:bedrock'
+              "block": "minecraft:bedrock"
             }
           }
         ],
@@ -141,7 +127,7 @@ ServerEvents.recipes(event => {
             "type": "has_block",
             "match_block": {
               "blocks": [
-                'spectrum:ruin'
+                "spectrum:ruin"
               ],
               "state": {
                 "conversion": "default"
