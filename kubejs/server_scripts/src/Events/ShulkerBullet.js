2e0a314a63b3139d3e91319187c20e63b68a8fb4
hao888myt <1581375158@qq.com>
2025-09-30 21:52:58 +0800

    优化了随机代码

--- kubejs/server_scripts/src/Events/ShulkerBullet.js
+++ kubejs/server_scripts/src/Events/ShulkerBullet.js
@@ -101,17 +101,6 @@ NativeEvents.onEvent(Java.loadClass('net.minecraftforge.event.entity.ProjectileI
         return result
     }
 
-    function randomNum(minNum, maxNum){ 
-        switch(arguments.length){ 
-            case 1: 
-                return parseInt(Math.random() * minNum + 1, 10)
-            case 2: 
-                return parseInt(Math.random() * (maxNum - minNum + 1) + minNum, 10)
-            default: 
-                return 0;
-        } 
-    } 
-
     function parseBlockState(input) {
         const pattern = /^Block{([^}]+)}(?:\[(.*)])?$/;
         const match = input.match(pattern);
--- kubejs/server_scripts/src/Events/Toretoise.js
+++ kubejs/server_scripts/src/Events/Toretoise.js
@@ -85,14 +85,3 @@ function hasOreTypeZero(str) {
 function mergeOreType(toretoise, num){
     toretoise.mergeNbt(`{oreType: ${num}}`)
 }
-
-function randomNum(minNum, maxNum){ 
-        switch(arguments.length){ 
-            case 1: 
-                return parseInt(Math.random() * minNum + 1, 10)
-            case 2: 
-                return parseInt(Math.random() * (maxNum - minNum + 1) + minNum, 10)
-            default: 
-                return 0;
-        } 
-    } 
