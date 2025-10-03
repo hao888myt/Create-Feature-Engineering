74ebdca6a6968d578b6de1dbeca225fe227490d5
hao888myt <1581375158@qq.com>
2025-10-03 19:21:31 +0800

    提高了矿石饲料的产率

--- kubejs/server_scripts/src/Recipes/Create/Mixing.js
+++ kubejs/server_scripts/src/Recipes/Create/Mixing.js
@@ -8,18 +8,18 @@ ServerEvents.recipes(event=>{
     create.mixing(Fluid.of("createmetallurgy:molten_steel", 180), [Item.of("minecraft:coal", 3), Fluid.of("createmetallurgy:molten_iron", 360)]).heated()
 
     //植本质
-    create.mixing(Item.of("create:tree_fertilizer", 4), [Item.of("minecraft:bone_meal", 2), "spectrum:vegetal"])
+    create.mixing(Item.of("create:tree_fertilizer", 8), [Item.of("minecraft:bone_meal", 4), "spectrum:vegetal"])
 
     // 矿石饲料
     create.mixing([
-        Item.of("kubejs:ore_feed", 8), 
-        Item.of("kubejs:ore_feed", 2).withChance(0.5)], 
+        Item.of("kubejs:ore_feed", 16), 
+        Item.of("kubejs:ore_feed", 8).withChance(0.5)], 
         [
-            Item.of("minecraft:coal", 2), 
-            Item.of("spelunkery:rough_cinnabar"), 
-            Item.of("create:crushed_raw_iron"), 
-            Item.of("spelunkery:rough_lazurite", 5), 
-            Item.of("create:crushed_raw_copper"), 
-            Item.of("minecraft:glow_berries", 8)
+            Item.of("minecraft:coal", 4), 
+            Item.of("spelunkery:rough_cinnabar", 2), 
+            Item.of("create:crushed_raw_iron", 2), 
+            Item.of("spelunkery:rough_lazurite", 10), 
+            Item.of("create:crushed_raw_copper", 2), 
+            Item.of("minecraft:glow_berries", 4)
         ])
 })
