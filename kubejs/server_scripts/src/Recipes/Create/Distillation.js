00980aa9cdb82ebc09e76e57c25ae960ea203072
hao888myt <1581375158@qq.com>
2025-10-04 13:17:59 +0800

    石油化工

--- kubejs/server_scripts/src/Recipes/Create/Distillation.js
+++ kubejs/server_scripts/src/Recipes/Create/Distillation.js
@@ -4,15 +4,35 @@ ServerEvents.recipes(event => {
     // 原油分馏
     event.remove("createdieselgenerators:distillation/superheated_crude_oil")
     event.remove("createdieselgenerators:distillation/crude_oil")
+    // 加热
     cdg.distillation([
         Fluid.of("kubejs:heavy_oil", 50),
-        Fluid.of("kubejs:liquefied_petroleum_gas", 75),
-        Fluid.of("kubejs:light_oil", 50)
+        Fluid.of("kubejs:light_oil", 50),
+        Fluid.of("kubejs:liquefied_petroleum_gas", 75)
     ],  Fluid.of("createdieselgenerators:crude_oil", 100)).heated()
-
+    // 超级加热
     cdg.distillation([
         Fluid.of("kubejs:heavy_oil", 100),
-        Fluid.of("kubejs:liquefied_petroleum_gas", 150),
-        Fluid.of("kubejs:light_oil", 100)
+        Fluid.of("kubejs:light_oil", 100),
+        Fluid.of("kubejs:liquefied_petroleum_gas", 150)
     ],  Fluid.of("createdieselgenerators:crude_oil", 100)).superheated()
+
+
+    // 轻油分馏
+    cdg.distillation([
+        Fluid.of("createdieselgenerators:diesel", 50), 
+        Fluid.of("createdieselgenerators:gasoline", 50)
+    ], Fluid.of("kubejs:light_oil", 100)).heated()
+
+    // 轻油裂解
+    cdg.distillation([
+        Fluid.of("kubejs:liquefied_petroleum_gas", 75)
+    ], Fluid.of("kubejs:light_oil", 100)).superheated()
+
+    
+    // 重油裂解
+    cdg.distillation([
+        Fluid.of("kubejs:light_oil", 75),
+        Fluid.of("kubejs:liquefied_petroleum_gas", 100)
+    ], Fluid.of("kubejs:heavy_oil", 100)).superheated()
 })
