c31fb036a7ea9a0f81fd21dd6a74db0c63020381
hao888myt <1581375158@qq.com>
2025-10-04 12:56:58 +0800

    修复了拼写错误

--- kubejs/server_scripts/src/Recipes/Create/Distillation.js
+++ kubejs/server_scripts/src/Recipes/Create/Distillation.js
@@ -2,15 +2,17 @@ ServerEvents.recipes(event => {
     const cdg = event.recipes.createdieselgenerators
 
     // 原油分馏
+    event.remove("createdieselgenerators:distillation/superheated_crude_oil")
+    event.remove("createdieselgenerators:distillation/crude_oil")
     cdg.distillation([
         Fluid.of("kubejs:heavy_oil", 50),
-        fluid.of("kubejs:liquefied_petroleum_gas", 75),
+        Fluid.of("kubejs:liquefied_petroleum_gas", 75),
         Fluid.of("kubejs:light_oil", 50)
     ],  Fluid.of("createdieselgenerators:crude_oil", 100)).heated()
 
     cdg.distillation([
         Fluid.of("kubejs:heavy_oil", 100),
-        fluid.of("kubejs:liquefied_petroleum_gas", 150),
+        Fluid.of("kubejs:liquefied_petroleum_gas", 150),
         Fluid.of("kubejs:light_oil", 100)
     ],  Fluid.of("createdieselgenerators:crude_oil", 100)).superheated()
 })
