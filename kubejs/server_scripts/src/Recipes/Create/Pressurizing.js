f518ae73d4bb2d4e1d4813ea0fdcdf8a3676b319
hao888myt <1581375158@qq.com>
2025-10-04 13:25:06 +0800

    润滑油

--- kubejs/server_scripts/src/Recipes/Create/Pressurizing.js
+++ kubejs/server_scripts/src/Recipes/Create/Pressurizing.js
@@ -1,4 +1,10 @@
 ServerEvents.recipes(event => {
     const vintageimprovements = event.recipes.vintageimprovements
-    vintageimprovements.pressurizing(Item.of("createmetallurgy:coke").withChance(0.75), "minecraft:coal").heated()
+    // 焦炭配方
+    vintageimprovements.pressurizing(Item.of("createmetallurgy:coke").withChance(0.75), 
+    "minecraft:coal").heated()
+
+    // 润滑油
+    vintageimprovements.pressurizing(Fluid.of("kubejs:lubricant", 50), 
+    Fluid.of("createdieselgenerators:diesel", 100)).heated()
 })
