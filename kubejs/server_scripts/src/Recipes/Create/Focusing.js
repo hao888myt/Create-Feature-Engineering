655a3755631e61a58c0ea36c3711ef8590646ad3
hao888myt <1581375158@qq.com>
2025-10-04 23:32:16 +0800

    单晶硅板配方

---
+++ kubejs/server_scripts/src/Recipes/Create/Focusing.js
@@ -0,0 +1,13 @@
+ServerEvents.recipes(event => {
+    const create_optical = event.recipes.create_optical
+
+    // required_beam_type
+    // 0 - 无线电波
+    // 1 - 微波
+    // 2 - 可见光
+    // 3 - 伽马射线
+    // 4 - 不需要光线
+
+    // 单晶硅板
+    create_optical.focusing("minecraft:chest", 20, 2, "kubejs:silicon_plate")
+})
