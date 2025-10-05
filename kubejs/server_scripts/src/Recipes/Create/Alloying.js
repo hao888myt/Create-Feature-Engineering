31a02395e2f31c21ee2b1d33be7f8b4c75b466f9
hao888myt <1581375158@qq.com>
2025-10-05 21:51:16 +0800

    钕合金现在需要加热了

--- kubejs/server_scripts/src/Recipes/Create/Alloying.js
+++ kubejs/server_scripts/src/Recipes/Create/Alloying.js
@@ -6,12 +6,12 @@ ServerEvents.recipes(event => {
         [
             Item.of("alexscaves:raw_azure_neodymium", 1),
             Fluid.of("createmetallurgy:molten_iron", 135)
-    ])
+    ]).heatRequirement("heated")
 
     // 熔融赤钕合金
     createmetallurgy.alloying(Fluid.of("kubejs:molten_scarlet_neodymium", 90), 
         [
             Item.of("alexscaves:raw_scarlet_neodymium", 1),
             Fluid.of("createmetallurgy:molten_iron", 135)
-    ])
+    ]).heatRequirement("heated")
 })
