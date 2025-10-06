07c9d7d1e01c03e06b23d9298ddc606d5adaf1a1
hao888myt <1581375158@qq.com>
2025-10-06 13:21:31 +0800

    单晶硅板

--- kubejs/server_scripts/src/Recipes/Create/Pressurizing.js
+++ kubejs/server_scripts/src/Recipes/Create/Pressurizing.js
@@ -1,10 +1,14 @@
 ServerEvents.recipes(event => {
     const vintageimprovements = event.recipes.vintageimprovements
-    // 焦炭配方
+    // 焦炭
     vintageimprovements.pressurizing(Item.of("createmetallurgy:coke").withChance(0.75), 
     "minecraft:coal").heated()
 
     // 润滑油
     vintageimprovements.pressurizing(Fluid.of("kubejs:lubricant", 50), 
     Fluid.of("kubejs:heavy_oil", 100)).heated()
+
+    // 单晶硅板
+    vintageimprovements.pressurizing(Item.of("kubejs:silicon_plate").withChance(0.5), 
+    "#spectrum:gemstone_shards").heated()
 })
