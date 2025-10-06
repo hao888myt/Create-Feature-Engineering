9008f2803866dedba6687fed2175b0935023c5f5
hao888myt <1581375158@qq.com>
2025-10-06 13:21:52 +0800

    水晶块tag

--- kubejs/server_scripts/src/Tags/ItemTags.js
+++ kubejs/server_scripts/src/Tags/ItemTags.js
@@ -1,21 +1,21 @@
-ServerEvents.tags('item', event => {
-    event.add('forge:ores_in_ground/deepslate', 'anvilcraft:deepslate_zinc_ore')
-    event.add('forge:ores_in_ground/deepslate', 'anvilcraft:deepslate_tin_ore')
-    event.add('lychee:fire_immune', 'spectrum:bedrock_dust')
-    event.add('lychee:fire_immune', 'spectrum:shimmerstone_gem')
+ServerEvents.tags("item", event => {
+    event.add("forge:ores_in_ground/deepslate", "anvilcraft:deepslate_zinc_ore")
+    event.add("forge:ores_in_ground/deepslate", "anvilcraft:deepslate_tin_ore")
+    event.add("lychee:fire_immune", "spectrum:bedrock_dust")
+    event.add("lychee:fire_immune", "spectrum:shimmerstone_gem")
 
     //添加催化剂标签
-    event.add('kubejs:catalysts', 'kubejs:paltaeria_catalyst')
-    event.add('kubejs:catalysts', 'kubejs:stratine_catalyst')
-    event.add('kubejs:catalysts', 'kubejs:hover_catalyst')
-    event.add('kubejs:catalysts', 'kubejs:gravity_catalyst')
+    event.add("kubejs:catalysts", "kubejs:paltaeria_catalyst")
+    event.add("kubejs:catalysts", "kubejs:stratine_catalyst")
+    event.add("kubejs:catalysts", "kubejs:hover_catalyst")
+    event.add("kubejs:catalysts", "kubejs:gravity_catalyst")
 
     //添加橡胶标签
-    event.add('forge:rubber', 'kubejs:rubber')
+    event.add("forge:rubber", "kubejs:rubber")
 
     //添加树脂标签
-    event.add('kubejs:sap', 'anvilcraft:resin')
-    event.add('kubejs:sap', 'alexscaves:pewen_sap')
+    event.add("kubejs:sap", "anvilcraft:resin")
+    event.add("kubejs:sap", "alexscaves:pewen_sap")
 
     //添加流体储罐标签
     let tank = [
@@ -60,6 +60,17 @@ ServerEvents.tags('item', event => {
     ]
     unstackable_numbers.forEach(unstackable_number => {
         event.add("kubejs:unstackable_numbers", unstackable_number)
+    })
+
+    let crystal_blocks = [
+        "minecraft:amethyst_block", 
+        "spectrum:citrine_block", 
+        "spectrum:topaz_block", 
+        "spectrum:onyx_block", 
+        "spectrum:moonstone_block"
+    ]
+    crystal_blocks.forEach(crystal_block => {
+        event.add("kubejs:crystal_blocks", crystal_block)
     })
 
     /**
