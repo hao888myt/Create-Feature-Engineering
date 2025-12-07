ddb6d938753b69551c2c45244d407145f9d84a53
hao888myt <1581375158@qq.com>
2025-12-07 22:49:54 +0800

    方铅岩配方修改

--- kubejs/server_scripts/src/Recipes/Create/Compacting.js
+++ kubejs/server_scripts/src/Recipes/Create/Compacting.js
@@ -2,14 +2,13 @@ ServerEvents.recipes(event => {
     const create = event.recipes.create
 
     //方铅岩压块
-    event.remove('create_oppenheimered:compacting/galena')
-    create.compacting(['alexscaves:galena'], [Fluid.lava(25), '#c:lead_nuggets', 'minecraft:stone'])
+    create.compacting([Item.of("alexscaves:galena", 8)], [Fluid.lava(250), Item.of("anvilcraft:deepslate_chips", 8)])
 
     //结晶树液
-    // create.compacting(['create_dd:crystallized_sap', Item.of('createdieselgenerators:wood_chip', 8)], [Item.of('#create_dd:rubber_logs', 8)])
+    // create.compacting(["create_dd:crystallized_sap", Item.of("createdieselgenerators:wood_chip", 8)], [Item.of("#create_dd:rubber_logs", 8)])
 
     //植本质压骨粉
-    create.compacting(Item.of('minecraft:bone_meal', 16), 'spectrum:vegetal')
+    create.compacting(Item.of("minecraft:bone_meal", 16), "spectrum:vegetal")
 
     // 删除种子油配方
     event.remove("createaddition:compacting/seed_oil")
