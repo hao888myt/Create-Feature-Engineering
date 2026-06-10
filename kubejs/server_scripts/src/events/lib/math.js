bf18eec63e8fe7998efd10874823429db66d9961
hao888myt <1581375158@qq.com>
2026-06-10 19:34:04 +0800

    新增hex到rgb的转换

--- kubejs/server_scripts/src/events/lib/math.js
+++ kubejs/server_scripts/src/events/lib/math.js
@@ -15,4 +15,19 @@ class MathTool {
                 return 0;
         }
     }
+
+    /**
+     * @param {string} hex 
+     * @returns 
+     */
+    static hexToRgb(hex) {
+        // 移除开头的 '#' 并处理缩写形式（如 #010）
+        let normalizedHex = hex.replace(/^#/, '');
+
+        let r = parseInt(normalizedHex.substring(0, 2), 16);
+        let g = parseInt(normalizedHex.substring(2, 4), 16);
+        let b = parseInt(normalizedHex.substring(4, 6), 16);
+
+        return [r, g, b];
+    }
 }
