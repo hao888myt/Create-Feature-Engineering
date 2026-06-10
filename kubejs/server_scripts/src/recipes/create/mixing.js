172a8e11f01e3d114166fdc135ad62eb8762c73d
hao888myt <1581375158@qq.com>
2026-06-10 12:51:16 +0800

    修复液态数字的配方

--- kubejs/server_scripts/src/recipes/create/mixing.js
+++ kubejs/server_scripts/src/recipes/create/mixing.js
@@ -6,8 +6,9 @@ ServerEvents.recipes(event => {
 
     //植本质
     create.mixing(Item.of("create:tree_fertilizer", 8), [Item.of("minecraft:bone_meal", 4), "spectrum:vegetal"])
+
     // 液态数字
-    create.mixing(Fluid.of("create_feature_engineering:liquid_number", 1000), ["8x #create_feature_engineering:number", Fluid.of("minecraft:water", 1000)])
+    create.mixing(Fluid.of("create_feature_engineering:liquid_number", 1000), [Ingredient.of("#create_feature_engineering:numbers", 8), Fluid.of("minecraft:water", 1000)])
 
     // 电解水
     create.mixing([Fluid.of("create_feature_engineering:hydrogen", 500), Fluid.of("create_feature_engineering:oxygen", 250), "anvilcraft:capacitor_empty"], [Fluid.of("minecraft:water", 500), "anvilcraft:capacitor"])
