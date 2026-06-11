6f0ecc3b2af6abe4333b7c048c189f7a442c5613
hao888myt <1581375158@qq.com>
2026-06-11 08:46:19 +0800

    更改冲压的相关配方注释

--- kubejs/server_scripts/src/recipes/create/compacting.js
+++ kubejs/server_scripts/src/recipes/create/compacting.js
@@ -1,9 +1,9 @@
 ServerEvents.recipes(event => {
     let create = event.recipes.create
     
-    //植本质压骨粉
+    // 植本质压骨粉
     create.compacting(Item.of("minecraft:bone_meal", 16), "spectrum:vegetal")
 
-    // 删除种子油配方
+    // 种子油配方
     create.compacting(Fluid.of("createdieselgenerators:plant_oil", 100), "4x #forge:seeds")
 })
