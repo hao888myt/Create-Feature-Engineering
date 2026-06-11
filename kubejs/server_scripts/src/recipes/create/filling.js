c46020a8cc576a6d17809ba3ff0bf1bde017d7c9
hao888myt <1581375158@qq.com>
2026-06-11 18:50:55 +0800

    修复了电子管的配方

--- kubejs/server_scripts/src/recipes/create/filling.js
+++ kubejs/server_scripts/src/recipes/create/filling.js
@@ -1,21 +1,7 @@
 ServerEvents.recipes(event => {
-    const create = event.recipes.create
+    let create = event.recipes.create
 
     //电子管
-    event.remove('create:crafting/materials/electron_tube')
-    //create.filling('create:electron_tube', [Fluid.of('createmetallurgy:molten_iron', 90), 'create:polished_rose_quartz'])
-
-    // 水晶注液
-    FillingCrystal("citrine")
-    FillingCrystal("topaz")
-    function FillingCrystal(mateirial){
-      create.filling(Item.of(`spectrum:small_${mateirial}_bud`), [Item.of(`spectrum:${mateirial}_shard`), Fluid.of("minecraft:water", 250)])
-      create.filling(Item.of(`spectrum:medium_${mateirial}_bud`), [Item.of(`spectrum:small_${mateirial}_bud`), Fluid.of("minecraft:water", 250)])
-      create.filling(Item.of(`spectrum:large_${mateirial}_bud`), [Item.of(`spectrum:medium_${mateirial}_bud`), Fluid.of("minecraft:water", 250)])
-      create.filling(Item.of(`spectrum:${mateirial}_cluster`), [Item.of(`spectrum:large_${mateirial}_bud`), Fluid.of("minecraft:water", 250)])
-    }
-    create.filling(Item.of(`minecraft:small_amethyst_bud`), [Item.of(`minecraft:amethyst_shard`), Fluid.of("minecraft:water", 250)])
-    create.filling(Item.of(`minecraft:medium_amethyst_bud`), [Item.of(`minecraft:small_amethyst_bud`), Fluid.of("minecraft:water", 250)])
-    create.filling(Item.of(`minecraft:large_amethyst_bud`), [Item.of(`minecraft:medium_amethyst_bud`), Fluid.of("minecraft:water", 250)])
-    create.filling(Item.of(`minecraft:amethyst_cluster`), [Item.of(`minecraft:large_amethyst_bud`), Fluid.of("minecraft:water", 250)])
+    event.remove("create:crafting/materials/electron_tube")
+    create.filling("create:electron_tube", [Fluid.of("create_feature_engineering:flowing_molten_iron", 90), "create:polished_rose_quartz"])
   })
