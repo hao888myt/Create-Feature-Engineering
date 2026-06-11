1f710999938862a8184d42f0aadf213267d4cdfc
hao888myt <1581375158@qq.com>
2026-06-11 17:20:33 +0800

    删除了沙砾、沙子、红沙、灵魂沙、冰、岩浆块的洗涤配方

--- kubejs/server_scripts/src/recipes/create/splashing.js
+++ kubejs/server_scripts/src/recipes/create/splashing.js
@@ -1,10 +1,17 @@
 ServerEvents.recipes(event => {
-    let { create } = event.recipes
-    // event.remove('create_dd:emptying/chromatic_waste')
-    // create.splashing(Item.of('create_dd:compound_base').withChance(0.25), 'kubejs:adulterated_compound_base')
-    event.remove("create:splashing/red_sand")
-    // create.splashing([
-    //     Item.of("spelunkery:raw_gold_nugget", 3).withChance(0.75), 
-    //     Item.of("minecraft:dead_bush").withChance(0.05)
-    // ], "minecraft:red_sand")
+    let create = event.recipes.create
+
+    let removes = [
+        "create:splashing/sand",
+        "create:splashing/red_sand",
+        "create:splashing/gravel",
+        "create:splashing/soul_sand",
+
+        "create:splashing/ice",
+        "create:splashing/magma_block"
+    ]
+
+    removes.forEach(remove => {
+        event.remove(remove)
+    })
 })
