881df160fc4f189eccc8bc8266af8e1f4c6c5db4
hao888myt <1581375158@qq.com>
2026-06-12 22:40:58 +0800

    删除了无用的配方，修复了传送带的配方

--- kubejs/server_scripts/src/recipes/minecraft/craft_table.js
+++ kubejs/server_scripts/src/recipes/minecraft/craft_table.js
@@ -34,23 +34,6 @@ ServerEvents.recipes(event => {
     })
   })
 
-  // 超越维度
-  let beyond_dimension_remove = [
-    "beyonddimensions:ars_source_pathway",
-    "beyonddimensions:net_interface",
-    "beyonddimensions:net_pathway",
-    "beyonddimensions:net_energy_pathway",
-    "beyonddimensions:net_terminal_block",
-    "beyonddimensions:net_pump_block",
-    "beyonddimensions:net_hopper_block",
-    "beyonddimensions:net_furnace_block",
-    "beyonddimensions:dimensional_connect_block",
-    "beyonddimensions:net_control"
-  ]
-  beyond_dimension_remove.forEach(item => {
-    event.remove({ output: item })
-  })
-
   // 黄铜手部零件
   event.shaped(
     Item.of("create:brass_hand"),
@@ -60,38 +43,8 @@ ServerEvents.recipes(event => {
       " A "
     ],
     {
-      A: "#forge:plates/gold",
+      A: "#c:plates/gold",
       B: "create:andesite_alloy"
-    }
-  )
-
-  // 安山构件
-  event.shaped(
-    Item.of("create_feature_engineering:andesite_mechanism"),
-    [
-      " A ",
-      "BCB",
-      " A "
-    ],
-    {
-      A: "create:large_cogwheel",
-      B: "create:cogwheel",
-      C: "create:andesite_alloy"
-    }
-  )
-
-  // 铜构件
-  event.shaped(
-    Item.of("create_feature_engineering:copper_mechanism"),
-    [
-      " B ",
-      "ACA",
-      " B "
-    ],
-    {
-      A: "#create_feature_engineering:sap",
-      B: "#forge:plates/copper",
-      C: "create_feature_engineering:andesite_mechanism"
     }
   )
 
@@ -109,22 +62,6 @@ ServerEvents.recipes(event => {
     }
   )
 
-  //交易置物台
-  //event.remove("trading_floor:trading_depot")
-  // event.shaped(
-  //   Item.of("trading_floor:trading_depot"),
-  //   [
-  //     " A ",
-  //     " B ",
-  //     " C "
-  //   ],
-  //   {
-  //     A: "#forge:storage_blocks/emerald",
-  //     B: "create_dd:integrated_circuit",
-  //     C: "create:depot"
-  //   }
-  // )
-
   // 传送带
   event.shaped(
     Item.of("create:belt_connector", 6),
@@ -134,7 +71,7 @@ ServerEvents.recipes(event => {
     ],
     {
       D: "minecraft:dried_kelp",
-      R: "#forge:rubber"
+      R: "#neoforge:rubber"
     }
   )
   event.shaped(
@@ -145,7 +82,7 @@ ServerEvents.recipes(event => {
     ],
     {
       D: "minecraft:dried_kelp",
-      R: "#forge:rubber"
+      R: "#neoforge:rubber"
     }
   )
 
@@ -163,78 +100,6 @@ ServerEvents.recipes(event => {
     }
   )
 
-  //各种机器
-  let machines = (machine, casing, mechanism) => {
-    event.shaped(
-      Item.of(machine),
-      [
-        "AAA",
-        "ABA",
-        "AAA"
-      ],
-      {
-        A: mechanism,
-        B: casing
-      }
-    )
-  }
-
-  machines("create_feature_engineering:andesite_machine", "create:andesite_casing", "create_feature_engineering:andesite_mechanism")
-  machines("create_feature_engineering:copper_machine", "create:copper_casing", "create_feature_engineering:copper_mechanism")
-  machines("create_feature_engineering:brass_machine", "create:brass_casing", "create:precision_mechanism")
-  machines("create_feature_engineering:sturdy_machine", "create:railway_casing", "create_feature_engineering:sturdy_mechanism")
-  machines("create_feature_engineering:magnet_machine", "create_feature_engineering:steel_casing", "create_feature_engineering:magnet_mechanism")
-
-  machines("create_feature_engineering:logistics_machine", "create:cardboard_block", "create_feature_engineering:logistics_mechanism")
-
-  //化合物基质
-  let item_to_block = (item, block, number) => {
-    if (number == 4) {
-      event.shaped(
-        block,
-        [
-          "II ",
-          "II ",
-          "   "
-        ],
-        {
-          I: item
-        }
-      )
-    }
-    else if (number == 9) {
-      event.shaped(
-        block,
-        [
-          "III",
-          "III",
-          "III"
-        ],
-        {
-          I: item
-        }
-      )
-    }
-    event.shapeless(
-      Item.of(item, number),
-      [block]
-    )
-  }
-
-  //螺旋桨
-  // event.remove("immersive_aircraft:propeller")
-  // event.shaped(
-  //   Item.of("immersive_aircraft:propeller"),
-  //   [
-  //     "AA ",
-  //     " A ",
-  //     " AA"
-  //   ],
-  //   {
-  //     A: "#forge:plates/iron"
-  //   }
-  // )
-
   // 玫瑰石英
   event.remove("create:crafting/materials/rose_quartz")
   event.shapeless(
@@ -244,64 +109,4 @@ ServerEvents.recipes(event => {
       Item.of("minecraft:redstone", 4)
     ]
   )
-
-  // 特斯拉球
-  event.remove("alexscaves:tesla_bulb")
-  event.shaped(
-    Item.of("alexscaves:tesla_bulb", 4),
-    [
-      "EEE",
-      "ETE",
-      "EEE"
-    ],
-    {
-      E: "alexscaves:energized_galena_neutral",
-      T: "alexscaves:telecore"
-    }
-  )
-
-  // 传动复制
-  function copy(item, mechanism) {
-    event.shapeless(
-      Item.of(item, 4),
-      [
-        item,
-        mechanism
-      ]
-    )
-  }
-
-  let andesite_copy = [
-    "create_connected:encased_chain_cogwheel",
-    "create:gearshift",
-    "create_connected:inverted_gearshift",
-    "create_connected:parallel_gearbox",
-    "create_connected:vertical_parallel_gearbox",
-    "create_connected:six_way_gearbox",
-    "create:encased_chain_drive",
-    "create_connected:cross_connector",
-    "create_connected:vertical_six_way_gearbox",
-    "create:gearbox",
-    "create:vertical_gearbox",
-    "create:chain_conveyor",
-    "create:powered_latch",
-    "create:powered_toggle_latch"
-  ]
-
-  andesite_copy.forEach(item => {
-    copy(item, "create_feature_engineering:andesite_mechanism")
-  })
-
-  let brass_copy = [
-    "create_connected:brass_gearbox",
-    "create_connected:vertical_brass_gearbox",
-    "create:adjustable_chain_gearshift",
-    "create:pulse_extender",
-    "create:pulse_repeater",
-    "create:pulse_timer"
-  ]
-
-  brass_copy.forEach(item => {
-    copy(item, "create:precision_mechanism")
-  })
 })
