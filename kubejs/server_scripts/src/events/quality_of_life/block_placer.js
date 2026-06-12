8c127249dd50889265984150056ee3e65bd4d4f1
hao888myt <1581375158@qq.com>
2026-06-12 13:08:02 +0800

    修复了方块放置器右键可交互方块会优先与方块交互问题

--- kubejs/server_scripts/src/events/quality_of_life/block_placer.js
+++ kubejs/server_scripts/src/events/quality_of_life/block_placer.js
@@ -1,3 +1,6 @@
+let $TriState = Java.loadClass("net.neoforged.neoforge.common.util.TriState")
+
+// 创造无冷却
 ItemEvents.rightClicked("create_fantasizing:block_placer", event => {
     let player = event.getPlayer()
 
@@ -6,4 +9,11 @@ ItemEvents.rightClicked("create_fantasizing:block_placer", event => {
     player.getServer().scheduleInTicks(1, () => {
         player.cooldowns.removeCooldown("create_fantasizing:block_placer")
     })
+})
+
+// 方块放置器右键不会与方块交互
+NativeEvents.onEvent(Java.loadClass("net.neoforged.neoforge.event.entity.player.PlayerInteractEvent$RightClickBlock"), event => {
+    if(event.getItemStack().getItem().toString() != "create_fantasizing:block_placer") return
+    event.setUseItem($TriState.TRUE)
+    event.setUseBlock($TriState.FALSE)
 })
