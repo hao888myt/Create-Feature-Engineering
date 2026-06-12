c01de9641b39c6ed29f549e39701a6e7b9509ad4
hao888myt <1581375158@qq.com>
2026-06-12 21:55:18 +0800

    新增了物流研究包的配方

--- kubejs/server_scripts/src/recipes/create/sequenced_assembly.js
+++ kubejs/server_scripts/src/recipes/create/sequenced_assembly.js
@@ -3,12 +3,21 @@ ServerEvents.recipes(event => {
 	let create = event.recipes.create
 	let transitionalItem = "minecraft:barrier"
 
+	// 物流研究包
+	transitionalItem = "create_feature_engineering:incomplete_research_pack"
+	create.sequenced_assembly(Item.of('researchd:research_pack[researchd:research_pack="create_feature_engineering:logistics"]', 2),
+	 "create:cardboard_block", [
+			create.deploying(transitionalItem, [transitionalItem, "create:belt_connector"]),
+			create.deploying(transitionalItem, [transitionalItem, "create:chain_conveyor"]),
+			create.deploying(transitionalItem, [transitionalItem, "create:andesite_funnel"]),
+	]).transitionalItem(transitionalItem).loops(1)
+
 	// 智能研究包
-	transitionalItem = "create:incomplete_precision_mechanism"
+	transitionalItem = "create_feature_engineering:incomplete_research_pack"
 	create.sequenced_assembly(Item.of('researchd:research_pack[researchd:research_pack="create_feature_engineering:smart"]', 2),
 	 "create:precision_mechanism", [
-			create.deploying(transitionalItem, [transitionalItem, "create_connected:empty_fan_catalyst"]),
+			create.deploying(transitionalItem, [transitionalItem, "create:transmitter"]),
 			create.deploying(transitionalItem, [transitionalItem, "create:electron_tube"]),
 			create.deploying(transitionalItem, [transitionalItem, "create:filter"]),
-	]).transitionalItem(transitionalItem).loops(1).id("create:sdasdasdasd")
+	]).transitionalItem(transitionalItem).loops(1)
 })
