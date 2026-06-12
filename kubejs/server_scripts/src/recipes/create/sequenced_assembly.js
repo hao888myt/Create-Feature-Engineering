b1c5a0367447bf3a12551cdb2cf75cd62b23386e
hao888myt <1581375158@qq.com>
2026-06-12 22:04:08 +0800

    新增了包裹研究包的配方

--- kubejs/server_scripts/src/recipes/create/sequenced_assembly.js
+++ kubejs/server_scripts/src/recipes/create/sequenced_assembly.js
@@ -28,5 +28,14 @@ ServerEvents.recipes(event => {
 			create.deploying(transitionalItem, [transitionalItem, "create:transmitter"]),
 			create.deploying(transitionalItem, [transitionalItem, "create:electron_tube"]),
 			create.deploying(transitionalItem, [transitionalItem, "create:filter"]),
-	]).transitionalItem(transitionalItem).loops(1)
+	]).transitionalItem(transitionalItem).loops(2)
+
+	// 包裹研究包
+	transitionalItem = "create_feature_engineering:incomplete_research_pack"
+	create.sequenced_assembly(Item.of('researchd:research_pack[researchd:research_pack="create_feature_engineering:package"]', 2),
+	 "fluidlogistics:waterproof_cardboard_block", [
+			create.deploying(transitionalItem, [transitionalItem, "create:stock_link"]),
+			create.deploying(transitionalItem, [transitionalItem, "create:packager"]),
+			create.deploying(transitionalItem, [transitionalItem, "create:package_frogport"]),
+	]).transitionalItem(transitionalItem).loops(2)
 })
