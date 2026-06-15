54c4441bb72581bd8bc34fe3127ce435d9680cf8
hao888myt <1581375158@qq.com>
2026-06-15 21:38:30 +0800

    修改了流体泵的配方

--- kubejs/server_scripts/src/recipes/create/item_application.js
+++ kubejs/server_scripts/src/recipes/create/item_application.js
@@ -17,7 +17,9 @@ ServerEvents.recipes((event) => {
         ["create:empty_blaze_burner", "minecraft:netherrack", "#c:plates/iron"],
 
         ["create:packager", "create:cardboard_block", "#c:ingots/andesite"],
-        ["create:package_frogport", "create_fantasizing:andesite_crate", "#c:slimeballs"]
+        ["create:package_frogport", "create_fantasizing:andesite_crate", "#c:slimeballs"],
+
+        ["fluidlogistics:fluid_pump", "create:propeller", "create:mechanical_pump"]
     ]
 
 
