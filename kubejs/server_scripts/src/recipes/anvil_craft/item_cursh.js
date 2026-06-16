1b71e99ecc90d340a0767a93c5f8b8a215d1ebf5
hao888myt <1581375158@qq.com>
2026-06-16 17:51:46 +0800

    修改了树脂的配方

--- kubejs/server_scripts/src/recipes/anvil_craft/item_cursh.js
+++ kubejs/server_scripts/src/recipes/anvil_craft/item_cursh.js
@@ -2,12 +2,12 @@ ServerEvents.recipes(event => {
     // const { $ItemIngredientPredicate, $ChanceItemStack } = require("@package/dev/anvilcraft/lib/recipe/component")
     let { anvilcraft } = event.recipes
 
-    // let removes = [
-    //     "anvilcraft:item_crush/wood_fiber"
-    // ].forEach(recipe => {
-    //     event.remove(recipe)
-    // })
-    
+    let removes = [
+        "anvilcraft:item_crush/wood_fiber"
+    ].forEach(recipe => {
+        event.remove(recipe)
+    })
+
     // anvilcraft.item_crush(
     //     $ItemIngredientPredicate.of("minecraft:iron_nugget").build(),
     //     [
@@ -15,4 +15,24 @@ ServerEvents.recipes(event => {
     //         $ChanceItemStack.of(Item.of("anvilcraft:resin"))
     //     ]
     // )
+
+    event.custom(
+        {
+            "type": "anvilcraft:item_crush",
+            "ingredients": [
+                {
+                    "items": "#minecraft:logs"
+                }
+            ],
+            "results": [
+                {
+                    "count": 2,
+                    "id": "anvilcraft:wood_fiber"
+                },
+                {
+                    "id": "anvilcraft:resin"
+                }
+            ]
+        }
+    )
 })
