4c39fd4d6640ce6c17dadec93e9831ad67e80885
hao888myt <1581375158@qq.com>
2026-06-16 16:58:06 +0800

    tag冲压配方（WIP）

---
+++ kubejs/server_scripts/src/recipes/anvil_craft/item_cursh.js
@@ -0,0 +1,18 @@
+ServerEvents.recipes(event => {
+    // const { $ItemIngredientPredicate, $ChanceItemStack } = require("@package/dev/anvilcraft/lib/recipe/component")
+    let { anvilcraft } = event.recipes
+
+    // let removes = [
+    //     "anvilcraft:item_crush/wood_fiber"
+    // ].forEach(recipe => {
+    //     event.remove(recipe)
+    // })
+    
+    // anvilcraft.item_crush(
+    //     $ItemIngredientPredicate.of("minecraft:iron_nugget").build(),
+    //     [
+    //         $ChanceItemStack.of(Item.of("anvilcraft:wood_fiber", 2)),
+    //         $ChanceItemStack.of(Item.of("anvilcraft:resin"))
+    //     ]
+    // )
+})
