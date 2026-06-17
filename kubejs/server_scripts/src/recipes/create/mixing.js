aa83f37b306ae701b42c90d3300c63d29609ff90
hao888myt <1581375158@qq.com>
2026-06-17 23:30:47 +0800

    添加了熔融钢的配方

--- kubejs/server_scripts/src/recipes/create/mixing.js
+++ kubejs/server_scripts/src/recipes/create/mixing.js
@@ -2,13 +2,18 @@ ServerEvents.recipes(event => {
     let create = event.recipes.create
 
     // 传动研究包
-    create.mixing(Item.of('researchd:research_pack[researchd:research_pack="create_feature_engineering:conveying"]', 2),
-        [
-            Item.of("create:shaft", 2),
-            Item.of("create:cogwheel", 2),
-            Item.of("create:large_cogwheel", 2),
-            Item.of("create:andesite_casing")
-        ]).id(`${global.ModPackId}:conveying`)
+    create.mixing(Item.of('researchd:research_pack[researchd:research_pack="create_feature_engineering:conveying"]', 2), [
+        Item.of("create:shaft", 2),
+        Item.of("create:cogwheel", 2),
+        Item.of("create:large_cogwheel", 2),
+        Item.of("create:andesite_casing")
+    ]).id(`${global.ModPackId}:conveying`)
+
+    // 熔融钢
+    create.mixing(Fluid.of("create_feature_engineering:molten_steel", 90), [
+        Item.of("minecraft:coal"),
+        Fluid.of("create_feature_engineering:molten_iron", 180)
+    ]).heated()
 
     // 纸浆
     create.mixing(Item.of("create:pulp", 4), [Item.of("anvilcraft:wood_fiber", 2), Fluid.of("minecraft:water", 250)])
