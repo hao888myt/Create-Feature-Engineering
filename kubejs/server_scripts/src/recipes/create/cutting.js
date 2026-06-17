55e81c16d55768f77c98c63ed79621d4527dffc9
hao888myt <1581375158@qq.com>
2026-06-17 22:23:21 +0800

    增加了安山合金切成传动杆配方的效率

--- kubejs/server_scripts/src/recipes/create/cutting.js
+++ kubejs/server_scripts/src/recipes/create/cutting.js
@@ -2,7 +2,8 @@ ServerEvents.recipes(event => {
     let { create } = event.recipes
 
     let recipes = [
-        [Item.of("create:fluid_pipe", 4), "#c:plates/copper", false]
+        [Item.of("create:fluid_pipe", 4), "#c:plates/copper", false],
+        [Item.of("create:shaft", 6), "#c:ingots/andesite"]
     ]
 
     recipes.forEach(recipe => {
