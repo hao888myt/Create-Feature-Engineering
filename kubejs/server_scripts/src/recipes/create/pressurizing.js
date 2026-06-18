c4e74e2b3bb2b9ab7ffd708313416c370988191d
hao888myt <1581375158@qq.com>
2026-06-18 12:35:06 +0800

    添加了黑曜石的配方

--- kubejs/server_scripts/src/recipes/create/pressurizing.js
+++ kubejs/server_scripts/src/recipes/create/pressurizing.js
@@ -7,4 +7,11 @@ ServerEvents.recipes(event => {
     // 单晶硅板
     vintageimprovements.pressurizing(CreateItem.of("create_feature_engineering:silicon_sheet", 0.5),
         Ingredient.of("#spectrum:gemstone_clusters")).heated()
+
+    // 黑曜石
+    vintageimprovements.pressurizing(Item.of("minecraft:obsidian", 2),
+        [
+            Fluid.of("minecraft:lava", 500),
+            Fluid.of("minecraft:water", 500)
+        ])
 })
