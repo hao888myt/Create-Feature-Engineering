68da416ea1f0115a9bc4dd2a5a9da86a3d6549ef
hao888myt <1581375158@qq.com>
2026-06-19 20:53:37 +0800

    紫晶傀儡可以转化光谱世界的水晶块变成母岩了

---
+++ kubejs/server_scripts/src/recipes/ars_nouveau/conversion.js
@@ -0,0 +1,16 @@
+ServerEvents.recipes(event => {
+    let conversions = [
+        ["spectrum:topaz_block", "spectrum:budding_topaz"],
+        ["spectrum:citrine_block", "spectrum:budding_citrine"],
+        ["spectrum:onyx_block", "spectrum:budding_onyx"],
+        ["spectrum:moonstone_block", "spectrum:budding_moonstone"]
+    ].forEach(conversion => {
+        event.custom(
+            {
+                "type": "ars_nouveau:budding_conversion",
+                "input": conversion[0],
+                "result": conversion[1]
+            }
+        )
+    })
+})
