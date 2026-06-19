145fb7f80a9fff25049d44abc4b3a202f8aacdf3
hao888myt <1581375158@qq.com>
2026-06-19 20:49:15 +0800

    让粉碎魔符兼容了部分铁砧工艺的配方

--- kubejs/server_scripts/src/recipes/ars_nouveau/crush.js
+++ kubejs/server_scripts/src/recipes/ars_nouveau/crush.js
@@ -1,25 +1,32 @@
 ServerEvents.recipes(event => {
     let ars_nouveau = event.recipes.ars_nouveau
 
-    // 霜冻水晶粉碎
-    ars_nouveau.crush(
-        "spectrum:frostbite_crystal",
-        [
-            {
-                stack: Item.of("spectrum:frostbite_essence", 16),
-                chance: 1.0,
-            }
-        ]
-    )
+    let removes = [
+        "ars_nouveau:cobblestone",
+        "ars_nouveau:stone"
+    ].forEach(recipe => {
+        event.remove(recipe)
+    })
 
-    // 炽热水晶粉碎
-    ars_nouveau.crush(
-        "spectrum:blazing_crystal", // input block
-        [
-            {
-                stack: Item.of("spectrum:incandescent_essence", 16),
-                chance: 1.0,
-            }
-        ]
-    )
+    let crushes = [
+        ["spectrum:frostbite_crystal", Item.of("spectrum:frostbite_essence", 16)],
+        ["spectrum:blazing_crystal", Item.of("spectrum:incandescent_essence", 16)],
+        ["minecraft:andesite", "anvilcraft:cinerite"],
+        ["minecraft:granite", "minecraft:red_sand"],
+        ["minecraft:diorite", "anvilcraft:quartz_sand"],
+        ["#c:cobblestones", "minecraft:gravel"],
+        ["minecraft:gravel", "minecraft:sand"],
+        ["minecraft:end_stone", "anvilcraft:end_dust"],
+        ["minecraft:netherrack", "anvilcraft:nether_dust"]
+    ].forEach(crush => {
+        ars_nouveau.crush(
+            crush[0],
+            [
+                {
+                    stack: crush[1],
+                    chance: 1.0
+                }
+            ]
+        )
+    })
 })
