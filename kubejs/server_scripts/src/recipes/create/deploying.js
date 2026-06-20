1308de84bcc4bd869e85b34474a82e518f7612b6
hao888myt <1581375158@qq.com>
2026-06-20 16:37:33 +0800

    添加了安山漏斗的机械手使用配方

--- kubejs/server_scripts/src/recipes/create/deploying.js
+++ kubejs/server_scripts/src/recipes/create/deploying.js
@@ -1,4 +1,11 @@
 ServerEvents.recipes(event => {
     let create = event.recipes.create
 
+    let recipes = [
+        [Item.of("create:andesite_funnel", 2), "#c:ingots/andesite", "minecraft:dried_kelp", false]
+    ]
+    recipes.forEach(recipe => {
+        create.deploying(recipe[0], [Ingredient.of(recipe[1]), Ingredient.of(recipe[2])])
+        removeRecipe(recipe, 3, event)
+    })
 })
