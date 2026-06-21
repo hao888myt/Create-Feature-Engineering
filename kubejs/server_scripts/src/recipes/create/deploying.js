f7ea2001d8d87ea49f0e59127b5df1e715aa0773
hao888myt <1581375158@qq.com>
2026-06-21 21:41:26 +0800

    feat（recipes）：添加了保险库和流体储罐的替代配方

--- kubejs/server_scripts/src/recipes/create/deploying.js
+++ kubejs/server_scripts/src/recipes/create/deploying.js
@@ -2,7 +2,9 @@ ServerEvents.recipes(event => {
     let create = event.recipes.create
 
     let recipes = [
-        [Item.of("create:andesite_funnel", 2), "#c:ingots/andesite", "minecraft:dried_kelp", false]
+        [Item.of("create:andesite_funnel", 2), "#c:ingots/andesite", "minecraft:dried_kelp", false],
+        ["create:fluid_tank", "minecraft:barrel", "#c:plates/copper", false],
+        ["create:item_vault", "minecraft:barrel", "#c:plates/iron", false]
     ]
     recipes.forEach(recipe => {
         create.deploying(recipe[0], [Ingredient.of(recipe[1]), Ingredient.of(recipe[2])])
