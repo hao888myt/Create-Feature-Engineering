33780e6a754e7c791b121ae24c8f09b1812017e3
hao888myt <1581375158@qq.com>
2026-06-21 19:31:49 +0800

    修改了橡胶的配方

--- kubejs/server_scripts/src/recipes/minecraft/smelting.js
+++
@@ -1,5 +0,0 @@
-ServerEvents.recipes(event =>{
-    
-    //橡胶配方
-    event.smelting("create_feature_engineering:rubber", "anvilcraft:resin").xp(0.1)
-})
---
+++ kubejs/server_scripts/src/recipes/minecraft/smoking.js
@@ -0,0 +1,5 @@
+ServerEvents.recipes(event =>{
+    
+    //橡胶配方
+    event.smoking("create_feature_engineering:rubber", "anvilcraft:resin").xp(0.1)
+})
