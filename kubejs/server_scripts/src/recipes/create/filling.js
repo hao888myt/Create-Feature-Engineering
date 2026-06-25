e9e53cc8f3d614a617e97c420bef5ce87966a77b
hao888myt <1581375158@qq.com>
2026-06-25 09:41:39 +0800

    style（recipes）：修改了filling的缩进

--- kubejs/server_scripts/src/recipes/create/filling.js
+++ kubejs/server_scripts/src/recipes/create/filling.js
@@ -1,12 +1,12 @@
 ServerEvents.recipes(event => {
-    let create = event.recipes.create
+  let create = event.recipes.create
 
-    let removes = [
-      "create:crafting/materials/electron_tube"
-    ].forEach(recipe => {
-      event.remove(recipe)
-    })
-
-    //电子管
-    create.filling("create:electron_tube", [Fluid.of("create_feature_engineering:molten_iron", 90), "create:polished_rose_quartz"])
+  let removes = [
+    "create:crafting/materials/electron_tube"
+  ].forEach(recipe => {
+    event.remove(recipe)
   })
+
+  //电子管
+  create.filling("create:electron_tube", [Fluid.of("create_feature_engineering:molten_iron", 90), "create:polished_rose_quartz"])
+})
