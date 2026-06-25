72e9f10225c4b82134550f72e07136cc7dc4fb77
hao888myt <1581375158@qq.com>
2026-06-25 09:40:40 +0800

    tweak（recipes）：修改了保险库和储罐的配方

--- kubejs/server_scripts/src/recipes/create/deploying.js
+++ kubejs/server_scripts/src/recipes/create/deploying.js
@@ -3,8 +3,8 @@ ServerEvents.recipes(event => {
 
     let recipes = [
         [Item.of("create:andesite_funnel", 2), "#c:ingots/andesite", "minecraft:dried_kelp", false],
-        ["create:fluid_tank", "minecraft:barrel", "#c:plates/copper", false],
-        ["create:item_vault", "minecraft:barrel", "#c:plates/iron", false]
+        ["create:fluid_tank", "create_fantasizing:andesite_crate", "#c:plates/copper", false],
+        ["create:item_vault", "create_fantasizing:andesite_crate", "#c:plates/iron", false]
     ]
     recipes.forEach(recipe => {
         create.deploying(recipe[0], [Ingredient.of(recipe[1]), Ingredient.of(recipe[2])])
