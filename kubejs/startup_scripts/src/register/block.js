ae36f6c06ccefdd2ca8879bcf7b481bb4d6dea30
hao888myt <1581375158@qq.com>
2026-06-26 22:50:27 +0800

    feat（register）：新增了存储方块的批量注册

--- kubejs/startup_scripts/src/register/block.js
+++ kubejs/startup_scripts/src/register/block.js
@@ -2,4 +2,16 @@ StartupEvents.registry("block", event => {
     event.create(`${global.ModPackId}:paltaeria_ore_catalyst`).hardness(5.0).stoneSoundType() // 苍绿催生体
     
     event.create(`${global.ModPackId}:stradpole_skeleton`).hardness(3.0).stoneSoundType() // 跨座蝌蚪遗骸
+
+    global.Materials.forEach((material) => {
+        material.types.forEach((type) => {
+            switch (type) {
+                case MaterialType.STORAGE:
+                    event.create(`${global.ModPackId}:${material.id}_block`)
+                        .tag(`c:storage_blocks`)
+                        .tag(`c:storage_blocks/${material.id}`)
+                    break
+            }
+        })
+    })
 })
