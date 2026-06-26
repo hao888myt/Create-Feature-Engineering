4ee24efc7688e69d748a2b2553603046e789a260
hao888myt <1581375158@qq.com>
2026-06-26 18:16:36 +0800

    fix（recipes）：修复了种子油的配方

--- kubejs/server_scripts/src/recipes/create/compacting.js
+++ kubejs/server_scripts/src/recipes/create/compacting.js
@@ -1,6 +1,12 @@
 ServerEvents.recipes(event => {
     let create = event.recipes.create
 
+    let removes = [
+        "createdieselgenerators:compacting/plant_oil"
+    ].forEach(recipe => {
+        event.remove(recipe)
+    })
+
     // 木头压树脂
     create.compacting([Item.of("anvilcraft:wood_fiber", 2), Item.of("anvilcraft:resin")], Ingredient.of("#minecraft:logs"))
     
@@ -8,5 +14,5 @@ ServerEvents.recipes(event => {
     create.compacting(Item.of("minecraft:bone_meal", 16), "spectrum:vegetal")
 
     // 种子油配方
-    create.compacting(Fluid.of("createdieselgenerators:plant_oil", 100), "4x #forge:seeds")
+    create.compacting(Fluid.of("createdieselgenerators:plant_oil", 100), Ingredient.of("#c:seeds"))
 })
