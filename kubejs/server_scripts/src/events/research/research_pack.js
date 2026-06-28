e6f52534b132db2371a53fe01f6c4165203a3743
hao888myt <1581375158@qq.com>
2026-06-28 11:46:52 +0800

    tweak（research）：调整了智能研究包的位置

--- kubejs/server_scripts/src/events/research/research_pack.js
+++ kubejs/server_scripts/src/events/research/research_pack.js
@@ -244,6 +244,19 @@ ResearchdEvents.registerResearches(event => {
         ]))
         .effect(unlockRecipe("create:kjs/create_hose_pulley"))
         .parent("fluid")
+
+    event.create("smart")
+        .translatableName("智能研究包")
+        .literalDescription("黄铜时代")
+        .iconStacks('researchd:research_pack[researchd:research_pack="create_feature_engineering:smart"]')
+        .method(consumePacks([
+            [rp.conveying, 64, 50],
+            [rp.logistics, 64, 50],
+            [rp.fluid, 64, 50]
+        ]))
+        .effect(unlockRecipe("create_feature_engineering:smart"))
+        .parent("fluid")
+
     event.create("mechanical_crafter")
         .translatableName("动力合成器")
         .literalDescription("慢工出细活")
