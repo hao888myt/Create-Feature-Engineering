8ede3befeb1d0a496565f5d322230aede9fb26f5
hao888myt <1581375158@qq.com>
2026-06-28 22:16:10 +0800

    feat（recipes）：修改了工厂仪表和红石请求器的配方

--- kubejs/server_scripts/src/recipes/create/deploying.js
+++ kubejs/server_scripts/src/recipes/create/deploying.js
@@ -1,11 +1,21 @@
 ServerEvents.recipes(event => {
     let create = event.recipes.create
 
+    let removes = [
+        "create:crafting/logistics/factory_gauge",
+        "create:crafting/logistics/redstone_requester"
+    ].forEach(recipe => {
+        event.remove(recipe)
+    })
+
     let recipes = [
         [Item.of("create:andesite_funnel", 2), "#c:ingots/andesite", "minecraft:dried_kelp", false],
         ["create:fluid_tank", "create_fantasizing:andesite_crate", "#c:plates/copper", false],
         ["create:item_vault", "create_fantasizing:andesite_crate", "#c:plates/iron", false],
-        ["create:transmitter", "#vintageimprovements:springs/copper", "minecraft:redstone"]
+        ["create:transmitter", "#vintageimprovements:springs/copper", "minecraft:redstone"],
+
+        [Item.of("create:factory_gauge", 4), "create:precision_mechanism", "create:stock_link",],
+        [Item.of("create:redstone_requester", 4), "create:item_vault", "create:stock_link",],
     ]
     recipes.forEach(recipe => {
         create.deploying(recipe[0], [Ingredient.of(recipe[1]), Ingredient.of(recipe[2])])
