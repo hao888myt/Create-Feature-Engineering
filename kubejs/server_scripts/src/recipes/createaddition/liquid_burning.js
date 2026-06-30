b34b35d47c834f5e97df2f30fad89dc5fed6cbd3
hao888myt <1581375158@qq.com>
2026-06-30 10:38:24 +0800

    feat（recipes）：新增了沼气超级燃烧的配方

--- kubejs/server_scripts/src/recipes/createaddition/liquid_burning.js
+++ kubejs/server_scripts/src/recipes/createaddition/liquid_burning.js
@@ -4,4 +4,16 @@ ServerEvents.recipes(event => {
     ].forEach(recipe => {
         event.remove(recipe)
     })
+
+    event.custom({
+        "type": "createaddition:liquid_burning",
+        "burn_time": 24000,
+        "ingredients": [{
+            "type": "neoforge:tag",
+            "amount": 1000,
+            "tag": "c:biogas"
+        }],
+        "results": [],
+        "superheated": true
+    })
 })
