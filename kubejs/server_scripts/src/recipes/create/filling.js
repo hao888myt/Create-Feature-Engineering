807731df1298a96da90b1d106ec095c2b7fb5877
hao888myt <1581375158@qq.com>
2026-07-02 11:02:30 +0800

    feat（recipes）：新增了有机堆肥注水的配方

--- kubejs/server_scripts/src/recipes/create/filling.js
+++ kubejs/server_scripts/src/recipes/create/filling.js
@@ -9,4 +9,7 @@ ServerEvents.recipes(event => {
 
   //电子管
   create.filling(Item.of("create:electron_tube", 4), [Fluid.of("create_feature_engineering:molten_iron", 180), "create:polished_rose_quartz"])
+
+  // 沃土
+  create.filling(Item.of("farmersdelight:rich_soil"), [Fluid.of("minecraft:water", 1000), "farmersdelight:organic_compost"])
 })
