1bbd225ef0db37faebbee97785fb09b040a86894
hao888myt <1581375158@qq.com>
2026-07-06 19:36:51 +0800

    feat(recipes): 配方修改
    修改了粉碎之魔符的配方

--- kubejs/server_scripts/src/recipes/ars_nouveau/glyph.js
+++ kubejs/server_scripts/src/recipes/ars_nouveau/glyph.js
@@ -2,7 +2,8 @@ ServerEvents.recipes(event => {
     let { ars_nouveau } = event.recipes
 
     let removes = [
-        "ars_nouveau:glyph_launch"
+        "ars_nouveau:glyph_launch",
+        "ars_nouveau:glyph_crush"
     ].forEach(recipe => {
         event.remove(recipe)
     })
@@ -14,5 +15,13 @@ ServerEvents.recipes(event => {
             Ingredient.of("#c:leathers"),
             "ars_nouveau:air_essence"
         ], 30
+    )
+
+    ars_nouveau.glyph("ars_nouveau:glyph_crush",
+        [
+            Ingredient.of("#minecraft:anvil"),
+            Ingredient.of("#spectrum:gemstone_shards"),
+            Ingredient.of("anvilcraft:crushing_table")
+        ], 50
     )
 })
