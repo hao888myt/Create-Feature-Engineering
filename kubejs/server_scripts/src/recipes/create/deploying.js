01c5b92b378fe2e6cb90a778e977117ea1d8fc14
hao888myt <1581375158@qq.com>
2026-07-15 20:12:57 +0800

    feat(recipes): 配方修改
    修改了各种铸模的配方

--- kubejs/server_scripts/src/recipes/create/deploying.js
+++ kubejs/server_scripts/src/recipes/create/deploying.js
@@ -3,7 +3,12 @@ ServerEvents.recipes(event => {
 
     let removes = [
         "create:crafting/logistics/factory_gauge",
-        "create:crafting/logistics/redstone_requester"
+        "create:crafting/logistics/redstone_requester",
+
+        "createdieselgenerators:cutting/bar_mold",
+        "createdieselgenerators:cutting/bowl_mold",
+        "createdieselgenerators:cutting/lines_mold",
+        "createdieselgenerators:cutting/chain_mold"
     ].forEach(recipe => {
         event.remove(recipe)
     })
@@ -21,7 +26,14 @@ ServerEvents.recipes(event => {
         [Item.of("create:redstone_requester", 4), "create:item_vault", "create:stock_link",],
 
         ["fluidlogistics:multi_fluid_tank", "create_fantasizing:andesite_crate", "#c:plates/brass", false],
-        [Item.of("create:smart_fluid_pipe", 4), "create:fluid_pipe", "create:electron_tube"]
+        [Item.of("create:smart_fluid_pipe", 4), "create:fluid_pipe", "create:electron_tube"],
+
+        ['createdieselgenerators:mold[createdieselgenerators:mold_type="kubejs:plate"]', "#c:plates/iron", "#c:plates", false],
+        ['createdieselgenerators:mold[createdieselgenerators:mold_type="kubejs:nugget"]', "#c:nuggets/iron", "#c:nuggets", false],
+        ['createdieselgenerators:mold[createdieselgenerators:mold_type="createdieselgenerators:bar"]', "#c:plates/iron", "#c:ingots", false],
+        ['createdieselgenerators:mold[createdieselgenerators:mold_type="createdieselgenerators:bowl"]', "#c:plates/iron", "minecraft:bowl", false],
+        ['createdieselgenerators:mold[createdieselgenerators:mold_type="createdieselgenerators:chain"]', "#c:plates/iron", "#c:chains", false],
+        ['createdieselgenerators:mold[createdieselgenerators:mold_type="createdieselgenerators:lines"]', "#c:plates/iron", "#c:rods", false]
     ]
     recipes.forEach(recipe => {
         create.deploying(recipe[0], [Ingredient.of(recipe[1]), Ingredient.of(recipe[2])])
