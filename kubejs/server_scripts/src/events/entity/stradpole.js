f5cc93027eb4f56b60c88b44c04cebd38364afff
hao888myt <1581375158@qq.com>
2026-08-03 22:43:28 +0800

    fix(event): 修复跨座蝌蚪遗骸无法生成的bug
    修改了检测的生物名称和检测坐标的偏移量

--- kubejs/server_scripts/src/events/entity/stradpole.js
+++ kubejs/server_scripts/src/events/entity/stradpole.js
@@ -1,9 +1,10 @@
-EntityEvents.death("alexsmobs:stradpole", event =>{
-    if (event.getSource().getType() != "anvil") return
+EntityEvents.death("alexsmobsup:stradpole", event =>{
+    let type = event.getSource().getType()
+    if (type != "anvil" && type != "fallingBlock") return
     let stradpole = event.entity
     let level = stradpole.level
     let pos = stradpole.getOnPos()
     let block_below = level.getBlock(pos.below(0))
-    if (block_below.id != "minecraft:netherrack") return
+    if (block_below.id != "minecraft:netherrack" && block_below.id != "anvilcraft:nether_dust") return
     level.setBlockAndUpdate(pos.below(0), Block.getBlock("create_feature_engineering:stradpole_skeleton").defaultBlockState())
 })
