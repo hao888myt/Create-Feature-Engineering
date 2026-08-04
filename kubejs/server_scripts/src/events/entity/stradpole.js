08ea234b84dae81ba6b912031a6183ef3b4310a2
hao888myt <1581375158@qq.com>
2026-08-05 00:53:47 +0800

    feat(event): 优化跨座蝌蚪砸死的检测
    现在跨座蝌蚪死亡后会检测下方3x2x3的区域了，虽然大部分时候只生效3x1x3

--- kubejs/server_scripts/src/events/entity/stradpole.js
+++ kubejs/server_scripts/src/events/entity/stradpole.js
@@ -4,7 +4,17 @@ EntityEvents.death("alexsmobsup:stradpole", event =>{
     let stradpole = event.entity
     let level = stradpole.level
     let pos = stradpole.getOnPos()
-    let block_below = level.getBlock(pos.below(0))
-    if (block_below.id != "minecraft:netherrack" && block_below.id != "anvilcraft:nether_dust") return
-    level.setBlockAndUpdate(pos.below(0), Block.getBlock("create_feature_engineering:stradpole_skeleton").defaultBlockState())
+    let has_transformed = false
+    for (let y = 0; y <= 1; y++) {
+        for (let x = -1; x<= 1; x++) {
+            for (let z = -1; z<= 1; z++) {
+                if (has_transformed) return
+                let current_pos = pos.below(y).west(x).north(z)
+                let block_below = level.getBlock(current_pos)
+                if (block_below.id != "minecraft:netherrack" && block_below.id != "anvilcraft:nether_dust") continue
+                level.setBlockAndUpdate(current_pos, Block.getBlock("create_feature_engineering:stradpole_skeleton").defaultBlockState())
+                has_transformed = true
+            }
+        }
+    }
 })
