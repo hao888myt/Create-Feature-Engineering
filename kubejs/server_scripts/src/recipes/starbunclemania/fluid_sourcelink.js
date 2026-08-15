bcf646a116d019ffb4f877d1333f6073c9947927
hao888myt <1581375158@qq.com>
2026-08-15 10:43:10 +0800

    feat(recipe): 流体魔源通道
    新增午夜溶液产魔配方

---
+++ kubejs/server_scripts/src/recipes/starbunclemania/fluid_sourcelink.js
@@ -0,0 +1,7 @@
+ServerEvents.recipes(event => {
+    event.custom({
+        "type": "starbunclemania:fluid_sourcelink",
+        "fluid": "spectrum:midnight_solution",
+        "mb_to_source_ratio": 20
+    })
+})
