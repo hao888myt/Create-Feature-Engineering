3afe89a55883d5a195cb908d0fbc57452c824e1a
hao888myt <1581375158@qq.com>
2026-08-22 18:36:18 +0800

    chore(recipe): 配方修改
    修改了深层锌矿石的矿物涌泉配方

--- kubejs/server_scripts/src/recipes/anvil_craft/mineral_fountain.js
+++ kubejs/server_scripts/src/recipes/anvil_craft/mineral_fountain.js
@@ -1,10 +1,24 @@
 ServerEvents.recipes(event => {
-    let { anvilcraft } = event.recipes
-
     let removes = [
         "anvilcraft:mineral_fountain_chance/void_stone_from_overworld",
-        "anvilcraft:mineral_fountain_chance/void_stone_from_the_end"
+        "anvilcraft:mineral_fountain_chance/void_stone_from_the_end",
+        "anvilcraft:mineral_fountain_chance/earth_core_shard_ore_from_overworld",
+
+        "anvilcraft:mineral_fountain/deepslate_zinc_ore"
     ].forEach(recipe => {
         event.remove(recipe)
+    })
+
+    event.custom({
+        "type": "anvilcraft:mineral_fountain",
+        "from_block": {
+            "blocks": "minecraft:deepslate"
+        },
+        "need_block": {
+            "blocks": "#c:storage_blocks/raw_zinc"
+        },
+        "to_block": {
+            "block": "create:deepslate_zinc_ore"
+        }
     })
 })
