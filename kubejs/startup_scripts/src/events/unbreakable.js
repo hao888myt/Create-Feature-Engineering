bc640fc1494fb295033548a527923788e99f83ad
hao888myt <1581375158@qq.com>
2026-08-27 19:33:38 +0800

    feat(event): 占卜杖现在为无法破坏

--- kubejs/startup_scripts/src/events/unbreakable.js
+++ kubejs/startup_scripts/src/events/unbreakable.js
@@ -1,7 +1,8 @@
 ItemEvents.modification(event => {
     let unbreakable = [
         "anvilcraft:magnet",
-        "waystones:warp_stone"
+        "waystones:warp_stone",
+        "ars_nouveau:dowsing_rod"
     ].forEach(id => {
         event.modify(id, item => {
             item.setUnbreakable()
