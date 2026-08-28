b8aea3d8c3ee5ef597395e93769c46eaab224318
hao888myt <1581375158@qq.com>
2026-08-28 12:04:53 +0800

    feat(recipe): 配方修改
    修改了工厂仪表的配方

--- kubejs/server_scripts/src/recipes/create/filling.js
+++ kubejs/server_scripts/src/recipes/create/filling.js
@@ -2,13 +2,17 @@ ServerEvents.recipes(event => {
   let create = event.recipes.create
 
   let removes = [
-    "create:crafting/materials/electron_tube"
+    "create:crafting/materials/electron_tube",
+    "create:crafting/logistics/factory_gauge"
   ].forEach(recipe => {
     event.remove(recipe)
   })
 
-  //电子管
+  // 电子管
   create.filling(Item.of("create:electron_tube", 4), [Fluid.of("create_feature_engineering:molten_iron", 180), "create:polished_rose_quartz"])
+
+  // 工厂仪表
+  create.filling(Item.of("create:factory_gauge"), [Fluid.of("create_feature_engineering:molten_brass", 45), "create:stock_link"])
 
   // 沃土
   create.filling(Item.of("farmersdelight:rich_soil"), [Fluid.of("minecraft:water", 250), "farmersdelight:organic_compost"])
