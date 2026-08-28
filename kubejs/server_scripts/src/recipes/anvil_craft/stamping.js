a248d7606af653249b1b515fc64b6b723fd389e4
hao888myt <1581375158@qq.com>
2026-08-28 18:42:07 +0800

    feat(recipe): 新增配方
    新增纸浆铁砧冲压成纸板的配方

--- kubejs/server_scripts/src/recipes/anvil_craft/stamping.js
+++ kubejs/server_scripts/src/recipes/anvil_craft/stamping.js
@@ -1,3 +1,15 @@
 ServerEvents.recipes((event) => {
-    
+    event.custom({
+        "type": "anvilcraft:stamping",
+        "ingredients": [
+            {
+                "items": "create:pulp"
+            }
+        ],
+        "results": [
+            {
+                "id": "create:cardboard"
+            }
+        ]
+    })
 });
