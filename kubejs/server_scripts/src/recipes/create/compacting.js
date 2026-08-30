b250a1342ecc8eb58511896731594bf14e166ae6
hao888myt <1581375158@qq.com>
2026-08-30 11:17:41 +0800

    feat(recipe): 新增配方
    甜菜压糖

--- kubejs/server_scripts/src/recipes/create/compacting.js
+++ kubejs/server_scripts/src/recipes/create/compacting.js
@@ -10,6 +10,9 @@ ServerEvents.recipes(event => {
 
     // 木头压树脂
     create.compacting([Item.of("anvilcraft:wood_fiber", 2), Item.of("anvilcraft:resin")], Ingredient.of("#minecraft:logs"))
+
+    // 甜菜压糖
+    create.compacting(["minecraft:sugar", CreateItem.of("minecraft:sugar", 0.5)], "minecraft:beetroot")
     
     // 植本质压骨粉
     create.compacting(Item.of("minecraft:bone_meal", 16), "spectrum:vegetal")
