92c0ce7efa10228960de63ba5391a6736884e208
hao888myt <1581375158@qq.com>
2026-09-05 23:20:33 +0800

    feat(recipe): 新增配方
    新增硬化树脂的石油气配方

--- kubejs/server_scripts/src/recipes/vintageimprovements/pressurizing.js
+++ kubejs/server_scripts/src/recipes/vintageimprovements/pressurizing.js
@@ -2,11 +2,15 @@ ServerEvents.recipes(event => {
     let vintageimprovements = event.recipes.vintageimprovements
     // 润滑油
     vintageimprovements.pressurizing(Fluid.of("create_feature_engineering:lubricant", 50),
-        Fluid.of("create_feature_engineering:heavy_oil", 100)).heated()
+        Fluid.of("create_feature_engineering:heavy_oil", 100))
+        .heated()
+        .processingTime(90)
 
     // 单晶硅板
     vintageimprovements.pressurizing(CreateItem.of("create_feature_engineering:silicon_sheet", 0.5),
-        Ingredient.of("#spectrum:gemstone_clusters")).heated()
+        Ingredient.of("#spectrum:gemstone_clusters"))
+        .heated()
+        .processingTime(90)
 
     // 黑曜石
     vintageimprovements.pressurizing(Item.of("minecraft:obsidian", 2),
@@ -14,4 +18,11 @@ ServerEvents.recipes(event => {
             Fluid.of("minecraft:lava", 500),
             Fluid.of("minecraft:water", 500)
         ])
+        .processingTime(90)
+
+    // 硬化树脂
+    vintageimprovements.pressurizing(Item.of("anvilcraft:hardend_resin", 4),
+        Fluid.of("create_feature_engineering:petroleum_gas", 200))
+        .heated()
+        .processingTime(90)
 })
