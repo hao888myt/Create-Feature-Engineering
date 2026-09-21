914f1f4de0042210d02d6c761f6c7e34b0bed209
hao888myt <1581375158@qq.com>
2026-09-21 13:00:12 +0800

    feat(recipe): 配方修改
    修改了树脂的配方，新增了胶合板塑形的配方

--- kubejs/server_scripts/src/recipes/create/compacting.js
+++ kubejs/server_scripts/src/recipes/create/compacting.js
@@ -11,12 +11,12 @@ ServerEvents.recipes(event => {
         event.remove(recipe)
     })
 
-    // 木头压树脂
-    create.compacting([Item.of("anvilcraft:wood_fiber", 2), Item.of("anvilcraft:resin")], Ingredient.of("#minecraft:logs"))
+    // 胶合板
+    create.compacting(Item.of("anvilcraft:plywood", 8), [Item.of("anvilcraft:wood_fiber", 2), Item.of("anvilcraft:resin")])
 
     // 甜菜压糖
     create.compacting(["minecraft:sugar", CreateItem.of("minecraft:sugar", 0.5)], "minecraft:beetroot")
-    
+
     // 植本质压骨粉
     create.compacting(Item.of("minecraft:bone_meal", 16), "spectrum:vegetal")
 
--- kubejs/server_scripts/src/recipes/create/pressing.js
+++ kubejs/server_scripts/src/recipes/create/pressing.js
@@ -3,4 +3,7 @@ ServerEvents.recipes(event => {
 
     // 树皮压纸
     create.pressing(Item.of("minecraft:paper"), Item.of("farmersdelight:tree_bark"))
+
+    // 木头压树脂
+    create.pressing([Item.of("anvilcraft:wood_fiber", 2), Item.of("anvilcraft:resin")], Ingredient.of("#minecraft:logs"))
 })
