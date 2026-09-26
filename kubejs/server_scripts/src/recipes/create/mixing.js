3f7ec6591204bf03d17cc4b8ee1701bc79d8cccb
hao888myt <1581375158@qq.com>
2026-09-26 22:30:56 +0800

    feat(recipe): 配方修改
    修改了盐的配方

--- kubejs/server_scripts/src/recipes/create/mixing.js
+++ kubejs/server_scripts/src/recipes/create/mixing.js
@@ -8,6 +8,7 @@ ServerEvents.recipes(event => {
         "createaddition:mixing/bioethanol",
 
         "ratatouille:mixing/residue_solidify",
+        "ratatouille:mixing/salt_from_boil",
     ].forEach(recipe => {
         event.remove(recipe)
     })
@@ -74,6 +75,7 @@ ServerEvents.recipes(event => {
 
     // 液态数字
     create.mixing(Fluid.of("create_feature_engineering:liquid_number", 1000), [Ingredient.of("#create_feature_engineering:numbers", 8), Fluid.of("minecraft:water", 1000)])
+    create.mixing(["ratatouille:boil_stone", "ratatouille:salt"], ["ratatouille:boil_stone", Fluid.of("minecraft:water", 1000)]).heated()
 
     // 电解水
     create.mixing([Fluid.of("anvilcraft:hydrogen", 500), Fluid.of("anvilcraft:oxygen", 250), "anvilcraft:capacitor_empty"], [Fluid.of("minecraft:water", 500), "anvilcraft:capacitor"])
